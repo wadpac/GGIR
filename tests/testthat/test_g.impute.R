d0791c260d985b821a7cb5101ca7ef04bf960e55
Vincent van Hees <vincentvanhees@gmail.com>
2016-12-12 11:18:22 +0100

    first testthat test added

---
+++ tests/testthat/test_g.impute.R
@@ -0,0 +1,8 @@
+library(GGIR)
+test_that("g.impute returns a non-empty dataframe", {
+  data(data.getmeta)
+  data(data.inspectfile)
+  data(data.calibrate)
+  IMP = g.impute(M = data.getmeta, I = data.inspectfile)
+  expect_equal(length(IMP),11)
+})
