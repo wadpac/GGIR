8b2511cad2a5703566cc650d010e5e97040e0492
joeheywood-CLS <joe.heywood@ncvo.org.uk>
2016-12-23 14:05:23 +0000

    Functions in CLS_time and alterations to g.getmeta to fix timestamps issue

--- R/CLS_time.R
+++ R/CLS_time.R
@@ -1,5 +1,5 @@
 
-# function
+
 getStartEnd <- function(d, startHour, inputFormat = "%d/%m/%Y", outputFormat = "%d/%m/%Y %H:%M:%S",
                         tz = "Europe/London") {
     # Args: d - date (without time) in correct format of inputFormat 
@@ -16,3 +16,35 @@ getStartEnd <- function(d, startHour, inputFormat = "%d/%m/%Y", outputFormat = "
     data.frame(start = as.character(startTimeObj - fiveMinutes, format = outputFormat),
                end = as.character(startTimeObj + oneDay, format = outputFormat), stringsAsFactors = FALSE)
 }
+
+
+
+getStartEndNumeric <- function(d, hhr, startHour = 4, inputFormat = "%d/%m/%Y") {
+    # gets page numbers for the start and end of day specified in arg d
+    # Args: d (character) date string using format of 
+    #       hhr: GENEActiv::header.info(f) output
+    #       startHour 
+    hcal <- attr(hhr, "calibration")
+    freq = as.numeric(hhr$Value[[2]])
+    tmzone <- ifelse(hcal$tz == 1, "Europe/London", "UTC")
+    nobs <- 300
+    timespan <- nobs/freq
+    timestamps <- seq(hcal$t1, by = timespan, length = hcal$npages)
+    d1 <- as.POSIXct(paste0(d, " 03:55:00"), format = "%d/%m/%Y %H:%M:%S", tz = "Europe/London")
+    dend <- d1 + (60*60*24) + (60*5) # (24 hours + five minutes)
+    start <- findInterval(d1 - 0.5, timestamps, all.inside = T)
+    end <- findInterval(dend, timestamps, all.inside = T)
+    # Value: data frame - one row, start page and end page
+    data.frame(start = start, end = end, stringsAsFactors = FALSE)
+}
+
+getFirstTimestamp <- function(f, p1) {
+    hhr <- header.info(f)
+    hcal <- attr(hhr, "calibration")
+    t1 <- hcal$t1
+    rate <- 1 / hcal$freq
+    nrows <- (p1 - t1) / rate
+    newRate <- 1 / as.numeric(hhr$Value[[2]])
+    as.POSIXct(t1 + (nrows * newRate), origin = "1970-1-1")
+}
+
