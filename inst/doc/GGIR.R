c5ea81b95e44982ae70ca778ea83f60f19d0bbec
Vincent van Hees <vincentvanhees@gmail.com>
2017-04-29 00:11:07 +0200

    vignette related

---
+++ inst/doc/GGIR.R
@@ -0,0 +1,9 @@
+## ----setup, include=FALSE------------------------------------------------
+knitr::opts_chunk$set(echo = TRUE)
+
+## ---- out.width = "700px",echo=FALSE-------------------------------------
+knitr::include_graphics("sleeplogexample.jpg")
+
+## ---- out.width = "700px",echo=FALSE-------------------------------------
+knitr::include_graphics("reportexample.jpg")
+
