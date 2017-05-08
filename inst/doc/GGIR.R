3c99d6d198fdbfb3508afacf4738e1b717241fee
Vincent van Hees <vincentvanhees@gmail.com>
2017-05-08 18:20:48 +0200

    vignette expansions

--- inst/doc/GGIR.R
+++ inst/doc/GGIR.R
@@ -7,3 +7,6 @@ knitr::include_graphics("sleeplogexample.jpg")
 ## ---- out.width = "700px",echo=FALSE-------------------------------------
 knitr::include_graphics("reportexample.jpg")
 
+## ---- out.width = "400px",echo=FALSE-------------------------------------
+knitr::include_graphics("nonwearimage.jpg")
+
