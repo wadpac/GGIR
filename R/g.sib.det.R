7de43e3274cf102f91c196f1ea94b9fa9a69351a
Vincent van Hees <vincentvanhees@gmail.com>
2017-05-09 15:47:11 +0200

    minor syntax edits

--- R/g.sib.det.R
+++ R/g.sib.det.R
@@ -55,6 +55,7 @@ g.sib.det = function(M,IMP,I,twd=c(-12,12),anglethreshold = 5,
         # count posture changes that happen less than once per ten minutes
         q1 = c()
         if (length(postch) > 1) {
+          
           q1 = which(diff(postch) > (i*(60/ws3))) #less than once per i minutes
         }
         if (length(q1) > 0) {
@@ -157,6 +158,7 @@ g.sib.det = function(M,IMP,I,twd=c(-12,12),anglethreshold = 5,
       cat("No midnights found")
       detection.failed = TRUE
     }
+
     metatmp = data.frame(time,invalid,night=night,sleep = sleep)
   } else {
     metatmp =c()
