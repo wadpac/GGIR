9d40e2340667ef16bfde7966265797634c897258
Vincent van Hees <vincentvanhees@gmail.com>
2017-07-24 14:22:33 +0200

    bug fixed in test_identify_levels

--- tests/testthat/test_identify_levels.R
+++ tests/testthat/test_identify_levels.R
@@ -12,14 +12,14 @@ test_that("identify_levels", {
                              boutdur.lig=5,boutcriter.lig=0.8,
                              boutdur.in=10,boutcriter.in=0.9,
                              ws3=5,bout.metric=4)
-  expect_that(table(levels$LEVELS)[1],equals(403))
-  expect_that(table(levels$LEVELS)[3],equals(553))
-  expect_that(table(levels$LEVELS)[5],equals(452))
-  expect_that(table(levels$LEVELS)[7],equals(617))
-  expect_that(table(levels$OLEVELS)[2],equals(639))
-  expect_that(table(levels$OLEVELS)[4],equals(787))
+  expect_that(as.numeric(table(levels$LEVELS))[1],equals(403))
+  expect_that(as.numeric(table(levels$LEVELS)[3]),equals(553))
+  expect_that(as.numeric(table(levels$LEVELS)[5]),equals(452))
+  expect_that(as.numeric(table(levels$LEVELS)[7]),equals(617))
+  expect_that(as.numeric(table(levels$OLEVELS)[2]),equals(639))
+  expect_that(as.numeric(table(levels$OLEVELS)[4]),equals(787))
   expect_that(levels$Lnames[12],equals("INB_D10T40"))
-  expect_that(table(as.numeric(levels$bc.in))[2],equals(1833))
+  expect_that(as.numeric(table(as.numeric(levels$bc.in))[2]),equals(1833))
   
 })
 
