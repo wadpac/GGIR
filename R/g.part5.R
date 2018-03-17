82d91805c928091ffd172a36a192c95037a129d5
Vincent van Hees <vincentvanhees@gmail.com>
2018-03-17 21:29:35 +0100

    Possible fix to issue #22 if L5HOUR cannot be detected then this does not kill the analyses but leaves the L5 value empty

--- R/g.part5.R
+++ R/g.part5.R
@@ -592,15 +592,24 @@ g.part5 = function(datadir=c(),metadatadir=c(),f0=c(),f1=c(),strategy=1,maxdur=7
                               if (length(unlist(strsplit(L5HOUR," "))) == 1) L5HOUR = paste0(L5HOUR," 00:00:00") #added because on some OS timestamps are deleted for midnight
                               if (length(unlist(strsplit(M5HOUR," "))) == 1) M5HOUR = paste0(M5HOUR," 00:00:00")
                             }
-                            time_num = sum(as.numeric(unlist(strsplit(unlist(strsplit(L5HOUR," "))[2],":"))) * c(3600,60,1)) / 3600
-                            if (time_num < 12) time_num = time_num + 24
-                            dsummary[di,fi] = time_num
+                          
+                            if (L5HOUR != "not detected") {
+                              time_num = sum(as.numeric(unlist(strsplit(unlist(strsplit(L5HOUR," "))[2],":"))) * c(3600,60,1)) / 3600
+                              if (time_num < 12) time_num = time_num + 24
+                              dsummary[di,fi] = time_num
+                            } else {
+                              dsummary[di,fi] = NA
+                            }
                           }
                           ds_names[fi] = paste("L",wini,"TIME_num",sep="");      fi = fi + 1
                           if (ignore == FALSE) {
-                            time_num = sum(as.numeric(unlist(strsplit(unlist(strsplit(M5HOUR," "))[2],":"))) * c(3600,60,1)) / 3600
-                            if (time_num < 12) time_num = time_num + 24
-                            dsummary[di,fi] = time_num
+                            if (M5HOUR != "not detected") {
+                              time_num = sum(as.numeric(unlist(strsplit(unlist(strsplit(M5HOUR," "))[2],":"))) * c(3600,60,1)) / 3600
+                              if (time_num < 12) time_num = time_num + 24
+                              dsummary[di,fi] = time_num
+                            } else {
+                              dsummary[di,fi] = NA
+                            }
                           }
                           ds_names[fi] = paste("M",wini,"TIME_num",sep="");      fi = fi + 1
                         }
