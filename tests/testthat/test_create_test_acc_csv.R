4d0a2665f0de22b70b9f262914f19a44e78e625f
Vincent van Hees <vincentvanhees@gmail.com>
2019-05-19 11:15:17 +0200

    updated istrue test to latest format

--- tests/testthat/test_create_test_acc_csv.R
+++ tests/testthat/test_create_test_acc_csv.R
@@ -3,6 +3,6 @@ context("create_test_acc_csv")
 test_that("create_test_acc_csv produces a file", {
   create_test_acc_csv()
   fn = "123A_testaccfile.csv"
-  expect_that(file.exists(fn),is_true())
+  expect_true(file.exists(fn))
   if (file.exists(fn)) file.remove(fn)
 })
