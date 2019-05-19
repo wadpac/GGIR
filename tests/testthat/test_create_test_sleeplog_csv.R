4360980a4b6aa6837091783bfe060e50db098a5e
Vincent van Hees <vincentvanhees@gmail.com>
2019-05-19 11:15:17 +0200

    updated istrue test to latest format

--- tests/testthat/test_create_test_sleeplog_csv.R
+++ tests/testthat/test_create_test_sleeplog_csv.R
@@ -3,6 +3,6 @@ context("create_test_sleeplog_csv")
 test_that("create_test_sleeplog_csv produces a file", {
   create_test_sleeplog_csv()
   fn = "testsleeplogfile.csv"
-  expect_that(file.exists(fn),is_true())
+  expect_true(file.exists(fn))
   if (file.exists(fn)) file.remove(fn)
 })
