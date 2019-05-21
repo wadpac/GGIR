c64c94487972bca409e028834275fc833b19af91
Vincent van Hees <vincentvanhees@gmail.com>
2019-05-21 13:29:23 +0200

    all argument names in read.myacc.csv extended with rmc. at the beginning to avoid namespace clashes as these arguments are passed on throughout GGIR

--- R/read.myacc.csv.R
+++ R/read.myacc.csv.R
@@ -1,39 +1,45 @@
-read.myacc.csv = function(file=c(), nrow=c(),  dec=".",
-                          firstrow.acc = 1, firstrow.header=c(),header.length = c(),
-                          col.acc = 1:3, col.temp = c(), col.time=c(),
-                          unit.acc = "g", unit.temp = "C", unit.time = "POSIX",
-                          format.time = "%Y-%m-%d %H:%M:%OS",
-                          bitrate = c(), dynamic_range = c(), unsignedbit = TRUE,
-                          origin = "1970-01-01",
-                          desiredtz = "Europe/London", samplefrequency = c(),
-                          headername.samplefrequency = c(),
-                          headername.deviceserialnumber = c(),
-                          headername.recordingid = c(),
-                          header.structure = c(),
-                          check4timegaps = FALSE) { # not included yet, optionally additonal columns
+read.myacc.csv = function(rmc.file=c(), rmc.nrow=c(), rmc.dec=".",
+                          rmc.firstrow.acc = 1, rmc.firstrow.header=c(),
+                          rmc.header.length = c(),
+                          rmc.col.acc = 1:3, rmc.col.temp = c(), rmc.col.time=c(),
+                          rmc.unit.acc = "g", rmc.unit.temp = "C", 
+                          rmc.unit.time = "POSIX",
+                          rmc.format.time = "%Y-%m-%d %H:%M:%OS",
+                          rmc.bitrate = c(), rmc.dynamic_range = c(),
+                          rmc.unsignedbit = TRUE,
+                          rmc.origin = "1970-01-01",
+                          rmc.desiredtz = "Europe/London", rmc.samplefrequency = c(),
+                          rmc.headername.samplefrequency = c(),
+                          rmc.headername.deviceserialnumber = c(),
+                          rmc.headername.recordingid = c(),
+                          rmc.header.structure = c(),
+                          rmc.check4timegaps = FALSE) { # not included yet, optionally additonal columns
   # bitrate should be or header item name as character, or the actual numeric bit rate
   # unit.temp can take C(elsius), F(ahrenheit), and K(elvin) and converts it into Celsius
-  if (length(firstrow.header) == 0) { # no header block
-    if (firstrow.acc == 2) {
+  # Note all argument names start with rmc (read myacc csv) to avoid name clashes when passed on throughout GGIR
+  if (length(rmc.firstrow.header) == 0) { # no header block
+    if (rmc.firstrow.acc == 2) {
       freadheader = TRUE
     } else {
       freadheader = FALSE
     }
-    skip = firstrow.acc
-    sf = samplefrequency
+    skip = rmc.firstrow.acc
+    sf = rmc.samplefrequency
     header = "no header"
   } else {
     # extract header information:
-    if (length(header.length) == 0) {
-      header.length = firstrow.acc-1
+    if (length(rmc.header.length) == 0) {
+      rmc.header.length = rmc.firstrow.acc-1
     }
     options(warn=-1) # fread complains about quote in first row for some file types
-    header_tmp = as.data.frame(data.table::fread(file,nrow = header.length, skip=firstrow.header-1,
-                                                 dec=dec, showProgress = FALSE, header = FALSE))
+    header_tmp = as.data.frame(data.table::fread(rmc.file,
+                                                 nrow = rmc.header.length, 
+                                                 skip=rmc.firstrow.header-1,
+                                                 dec=rmc.dec, showProgress = FALSE, header = FALSE))
     options(warn=0)
-    if (length(header.structure) != 0) { # header is stored in 1 column, with strings that need to be split
+    if (length(rmc.header.structure) != 0) { # header is stored in 1 column, with strings that need to be split
       if (length(header_tmp) == 1) { # one header item
-        header_tmp = as.matrix(unlist(strsplit(as.character(header_tmp[,1]), header.structure)))
+        header_tmp = as.matrix(unlist(strsplit(as.character(header_tmp[,1]), rmc.header.structure)))
       } else { # multiple header items
         if (ncol(header_tmp) > 1) {
           # collapse columns to one
@@ -43,7 +49,7 @@ read.myacc.csv = function(file=c(), nrow=c(),  dec=".",
           header_tmp = do.call(paste0, header_tmp)
         }
         mysplit = function(x){
-          tmp = strsplit(as.character(x),header.structure)
+          tmp = strsplit(as.character(x), rmc.header.structure)
           tmp = unlist(tmp)
           return(tmp)
         }
@@ -70,100 +76,100 @@ read.myacc.csv = function(file=c(), nrow=c(),  dec=".",
       colnames(header_tmp2) = NULL
       header = header_tmp2
     }
-    skip = firstrow.acc-1
+    skip = rmc.firstrow.acc-1
     freadheader = TRUE
     # assess whether accelerometer data conversion is needed
-    if (length(bitrate) > 0 & length(dynamic_range) > 0 & unit.acc == "bit") {
-      if (is.character(bitrate[1]) == TRUE) { # extract bitrate if it is in the header
-        bitrate = as.numeric(header[which(row.names(header) == bitrate[1]),1])
+    if (length(rmc.bitrate) > 0 & length(rmc.dynamic_range) > 0 & rmc.unit.acc == "bit") {
+      if (is.character(rmc.bitrate[1]) == TRUE) { # extract bitrate if it is in the header
+        rmc.bitrate = as.numeric(header[which(row.names(header) == rmc.bitrate[1]),1])
       }
-      if (dynamic_range[1] == "dynamic_range") { # extract dynamic range if it is in the header
-        dynamic_range = as.numeric(header[which(row.names(header) == dynamic_range[1]),1])
+      if (rmc.dynamic_range[1] == "dynamic_range") { # extract dynamic range if it is in the header
+        rmc.dynamic_range = as.numeric(header[which(row.names(header) == rmc.dynamic_range[1]),1])
       } 
     }
     # extract sample frequency:
-    sf = as.numeric(header[which(row.names(header) == headername.samplefrequency[1]),1])
-    sn = as.numeric(header[which(row.names(header) == headername.deviceserialnumber[1]),1])
-    id = as.numeric(header[which(row.names(header) == headername.recordingid[1]),1])
+    sf = as.numeric(header[which(row.names(header) == rmc.headername.samplefrequency[1]),1])
+    sn = as.numeric(header[which(row.names(header) == rmc.headername.deviceserialnumber[1]),1])
+    id = as.numeric(header[which(row.names(header) == rmc.headername.recordingid[1]),1])
     
     # standardise key header names to ease use elsewhere in GGIR:
-    if (length(headername.samplefrequency) > 0) {
-      row.names(header)[which(row.names(header) == headername.samplefrequency[1])] = "sample_rate"
+    if (length(rmc.headername.samplefrequency) > 0) {
+      row.names(header)[which(row.names(header) == rmc.headername.samplefrequency[1])] = "sample_rate"
     }
-    if (length(headername.deviceserialnumber) > 0) {
-      row.names(header)[which(row.names(header) == headername.deviceserialnumber[1])] = "device_serial_number"
+    if (length(rmc.headername.deviceserialnumber) > 0) {
+      row.names(header)[which(row.names(header) == rmc.headername.deviceserialnumber[1])] = "device_serial_number"
     }
-    if (length(headername.recordingid) > 0) {
-      row.names(header)[which(row.names(header) == headername.recordingid[1])] = "recordingID"
+    if (length(rmc.headername.recordingid) > 0) {
+      row.names(header)[which(row.names(header) == rmc.headername.recordingid[1])] = "recordingID"
     }
     if (length(sf) == 0) {
-      sf = samplefrequency # if sf not retrieved from header than use default
+      sf = rmc.samplefrequency # if sf not retrieved from header than use default
       header = rbind(header,1) # add it also to the header
       row.names(header)[nrow(header)] = "sample_rate"
     }
   }
   
   # read data from file
-  P = as.data.frame(data.table::fread(file,nrow = nrow, skip=skip,
-                                      dec=dec, showProgress = FALSE, header = freadheader))
+  P = as.data.frame(data.table::fread(rmc.file,nrow = rmc.nrow, skip=skip,
+                                      dec=rmc.dec, showProgress = FALSE, header = freadheader))
   # select relevant columns, add standard column names
-  P = P[,c(col.time,col.acc,col.temp)]
-  if (length(col.time) > 0 & length(col.temp) > 0) {
+  P = P[,c(rmc.col.time, rmc.col.acc, rmc.col.temp)]
+  if (length(rmc.col.time) > 0 & length(rmc.col.temp) > 0) {
     colnames(P) = c("timestamp","accx","accy","accz","temperature")
-  } else if (length(col.time) > 0 & length(col.temp) == 0) {
+  } else if (length(rmc.col.time) > 0 & length(rmc.col.temp) == 0) {
     colnames(P) = c("timestamp","accx","accy","accz")
-  } else if (length(col.time) == 0 & length(col.temp) > 0) {
+  } else if (length(rmc.col.time) == 0 & length(rmc.col.temp) > 0) {
     colnames(P) = c("accx","accy","accz","temperature")
-  } else if (length(col.time) == 0 & length(col.temp) == 0) {
+  } else if (length(rmc.col.time) == 0 & length(rmc.col.temp) == 0) {
     colnames(P) = c("accx","accy","accz")
   }
   # acceleration and temperature as numeric
   P$accx = as.numeric(P$accx)
   P$accy = as.numeric(P$accy)
   P$accz = as.numeric(P$accz)
-  if (length(col.temp) > 0) P$temperature = as.numeric(P$temperature) 
+  if (length(rmc.col.temp) > 0) P$temperature = as.numeric(P$temperature) 
   # Convert timestamps
-  if (length(col.time) > 0) {
-    if (unit.time == "POSIX") {
-      P$timestamp = as.POSIXlt(P$timestamp, origin=origin,tz = desiredtz)
-    } else if (unit.time == "UNIXsec") {
-      P$timestamp = as.POSIXlt(P$timestamp, origin=origin,tz = desiredtz)
-    } else if (unit.time == "ActivPAL") {
+  if (length(rmc.col.time) > 0) {
+    if (rmc.unit.time == "POSIX") {
+      P$timestamp = as.POSIXlt(P$timestamp, origin=rmc.origin,tz = rmc.desiredtz)
+    } else if (rmc.unit.time == "UNIXsec") {
+      P$timestamp = as.POSIXlt(P$timestamp, origin=rmc.origin,tz = rmc.desiredtz)
+    } else if (rmc.unit.time == "ActivPAL") {
       # origin should be specified as: "1899-12-30"
-      origin = "1899-12-30"
+      rmc.origin = "1899-12-30"
       datecode = round(P$timestamp) * 3600*24
       tmp2 = P$timestamp - round(P$timestamp)
       timecode = ((tmp2 * 10^10)*8.64) / 1000000
       numerictime = datecode +timecode
-      P$timestamp= as.POSIXlt(numerictime,origin=origin,tz=desiredtz)
+      P$timestamp= as.POSIXlt(numerictime,origin=rmc.origin,tz=rmc.desiredtz)
     }
   }
   # If acceleration is stored in mg units then convert to gravitational units
-  if (unit.acc == "mg") {
+  if (rmc.unit.acc == "mg") {
     P$accx = P$accx * 1000
     P$accy = P$accy * 1000
     P$accz = P$accz * 1000
   }
   # If acceleration is stored in bit values then convert to gravitational unit
-  if (length(bitrate) > 0 & length(dynamic_range) > 0 & unit.acc == "bit") {
-    if (unsignedbit == TRUE) {
-      P$accx = ((P$accx/(2^bitrate)) - 0.5) * 2*dynamic_range
-      P$accy = ((P$accy/(2^bitrate)) - 0.5) * 2*dynamic_range
-      P$accz = ((P$accz/(2^bitrate)) - 0.5) * 2*dynamic_range
-    } else if (unsignedbit == FALSE) { # signed bit
-      P$accx = (P$accx/((2^bitrate)/2)) * dynamic_range
-      P$accy = (P$accy/((2^bitrate)/2)) * dynamic_range
-      P$accz = (P$accz/((2^bitrate)/2)) * dynamic_range
+  if (length(rmc.bitrate) > 0 & length(rmc.dynamic_range) > 0 & rmc.unit.acc == "bit") {
+    if (rmc.unsignedbit == TRUE) {
+      P$accx = ((P$accx / (2^rmc.bitrate)) - 0.5) * 2* rmc.dynamic_range
+      P$accy = ((P$accy / (2^rmc.bitrate)) - 0.5) * 2* rmc.dynamic_range
+      P$accz = ((P$accz / (2^rmc.bitrate)) - 0.5) * 2* rmc.dynamic_range
+    } else if (rmc.unsignedbit == FALSE) { # signed bit
+      P$accx = (P$accx / ((2^rmc.bitrate)/2)) * rmc.dynamic_range
+      P$accy = (P$accy / ((2^rmc.bitrate)/2)) * rmc.dynamic_range
+      P$accz = (P$accz / ((2^rmc.bitrate)/2)) * rmc.dynamic_range
     }
   }
   # Convert temperature units
-  if (unit.temp == "K") {
+  if (rmc.unit.temp == "K") {
     P$temperature = P$temperature + 272.15 # From Kelvin to Celsius
-  } else if (unit.temp == "F") {
+  } else if (rmc.unit.temp == "F") {
     P$temperature = (P$temperature - 32) * (5/9) # From Fahrenheit to Celsius
   }
   # check for jumps in time and impute
-  if (check4timegaps == TRUE) {
+  if (rmc.check4timegaps == TRUE) {
     deltatime = abs(diff(P$timestamp))
     gapsi = which(deltatime > 1) # gaps indices
     newP = c()
