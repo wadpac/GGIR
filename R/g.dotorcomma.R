403a90279c53b0d76927bbb2bd97415158591197
Vincent van Hees <vincentvanhees@gmail.com>
2019-07-25 18:59:14 +0200

    minor changes: g.dotorcomma shortened, tests expanded and EN moved outside ENMO calculation to be available for MAD too

--- R/g.dotorcomma.R
+++ R/g.dotorcomma.R
@@ -1,51 +1,25 @@
 g.dotorcomma = function(inputfile,dformat,mon, desiredtz = c()) {
+  decn = "."
   if (dformat == 2) {
-    deci = read.csv(inputfile,skip = 100,nrow=10)
-    deci = as.matrix(deci)
-    if(is.na(suppressWarnings(as.numeric(deci[2,2]))) == T) {
-      decn = "," #comma
-    } else {
-      decn = "." #dot
-    }
-  } else if (dformat == 1) { #!decn is detect, but currently not used in the rest of the code!
+    deci = as.matrix(read.csv(inputfile,skip = 100,nrow=10))
+    if(is.na(suppressWarnings(as.numeric(deci[2,2]))) == T) decn = ","
+  } else if (dformat == 1) {
     if (mon == 1) {
-      try(expr={deci = g.binread(inputfile,0,2)},silent=TRUE)
-      deci = as.matrix(deci$rawxyz)
-      if(is.na(suppressWarnings(as.numeric(deci[2,2]))) == T) {
-        decn = "," #comma
-      } else {
-        decn = "." #dot
-      }
+      try(expr={deci = as.matrix(g.binread(inputfile,0,2))},silent=TRUE)
+      if(is.na(suppressWarnings(as.numeric(deci[2,2]))) == T) decn = ","
     } else if (mon == 2 ){
       if("GENEAread" %in% rownames(installed.packages()) == FALSE) {
         cat("\nWarning: R package GENEAread has not been installed, please install it before continuing")
       }
-      try(expr={deci = GENEAread::read.bin(binfile=inputfile,start=1,end=3,mmap.load=FALSE,calibrate=TRUE)},silent=TRUE)
-      deci = as.matrix(deci$data.out)
-      if(is.na(as.numeric(deci[2,2])) == T) {
-        decn = "," #comma
-      } else {
-        decn = "." #dot
-      }
+      try(expr={deci = as.matrix(GENEAread::read.bin(binfile=inputfile,start=1,end=3,mmap.load=FALSE,calibrate=TRUE))},silent=TRUE)
+      if(is.na(as.numeric(deci[2,2])) == T) decn = ","
     }
-  } else if (dformat == 3) { #!decn is detect, but currently not used in the rest of the code!
-    try(expr={deci = g.wavread(wavfile=inputfile,start=1,end=10)},silent=TRUE)
-    deci = as.matrix(deci$rawxyz)
-    if(is.na(suppressWarnings(as.numeric(deci[2,2]))) == T) {
-      decn = "," #comma
-    } else {
-      decn = "." #dot
-    }
-  } else if (dformat == 4) { #!decn is detect, but currently not used in the rest of the code!
-    # Rcpp::sourceCpp('src/numUnpack.cpp')
-    # Rcpp::sourceCpp('src/resample.cpp')
-    try(expr={deci = g.cwaread(inputfile,start = 1, end = 10, desiredtz = desiredtz)},silent=TRUE)
-    deci = as.matrix(deci$data)
-    if(is.na(suppressWarnings(as.numeric(deci[2,2]))) == T) {
-      decn = "," #comma
-    } else {
-      decn = "." #dot
-    }
+  } else if (dformat == 3) { 
+    try(expr={deci = as.matrix(g.wavread(wavfile=inputfile,start=1,end=10))},silent=TRUE)
+    if(is.na(suppressWarnings(as.numeric(deci[2,2]))) == T) decn = ","
+  } else if (dformat == 4) {
+    try(expr={deci =as.matrix(g.cwaread(inputfile,start = 1, end = 10, desiredtz = desiredtz))},silent=TRUE)
+    if(is.na(suppressWarnings(as.numeric(deci[2,2]))) == T) decn = ","
   }
   dotorcomma = decn
 }
