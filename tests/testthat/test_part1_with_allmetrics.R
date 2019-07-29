27d4b63445a72ab56088b379c1cafc04393ef257
Vincent van Hees <vincentvanhees@gmail.com>
2019-07-29 14:40:49 +0200

    typo fixed

--- tests/testthat/test_part1_with_allmetrics.R
+++ tests/testthat/test_part1_with_allmetrics.R
@@ -13,26 +13,28 @@ test_that("chainof5parts", {
   dn = "output_test"
   if (file.exists(dn))  unlink(dn,recursive=TRUE)
   g.part1(datadir=fn,outputdir=getwd(),f0=1,f1=1,overwrite=TRUE,desiredtz=desiredtz,
-          studyname="test",do.cal = FALSE, do.enmo = TRUE,do.lfenmo = TRUE,
+          studyname="test",do.cal = FALSE,
           # We are not doing all the metrics, because Travis-CI cannot allocate enough memory
-          # do.en = TRUE,
+          do.enmo = TRUE,do.lfenmo = TRUE,
           do.bfen = TRUE,
+          do.hfenplus = TRUE,
+          do.mad = TRUE,
+          # do.en = TRUE,
           # do.hfen=TRUE,
-          # do.hfenplus = TRUE, do.mad = TRUE,
-          do.anglex=TRUE,do.angley=TRUE,do.anglez=TRUE,
-          do.enmoa = TRUE,
+          # do.anglex=TRUE,do.angley=TRUE,do.anglez=TRUE,
+          # do.enmoa = TRUE,
           # do.roll_med_acc_x=TRUE,do.roll_med_acc_y=TRUE,do.roll_med_acc_z=TRUE,
-          do.dev_roll_med_acc_x=TRUE,#do.dev_roll_med_acc_y=TRUE,do.dev_roll_med_acc_z=TRUE,
+          # do.dev_roll_med_acc_x=TRUE,#do.dev_roll_med_acc_y=TRUE,do.dev_roll_med_acc_z=TRUE,
           # do.lfen = TRUE,
    windowsizes = c(15,3600,3600))
   rn = dir("output_test/meta/basic/",full.names = TRUE)
   load(rn[1])
-  expect_that(ncol(M$metashort),equals(19))
+  expect_that(ncol(M$metashort),equals(6))
   expect_that(nrow(M$metashort),equals(11280))
   expect_that(round(mean(M$metashort$BFEN),digits=4),equals(0.0302))
   expect_that(round(mean(M$metashort$LFENMO),digits=4),equals(0.0442))
   expect_that(round(mean(M$metashort$HFENplus),digits=4),equals(0.0791))
-  # expect_that(round(mean(M$metashort$dev_roll_med_acc_x),digits=4),equals(0.0067))
+  # # expect_that(round(mean(M$metashort$dev_roll_med_acc_x),digits=4),equals(0.0067))
   expect_that(round(mean(M$metashort$MAD),digits=4),equals(0.0072))
    
   if (file.exists(fn)) file.remove(fn)
