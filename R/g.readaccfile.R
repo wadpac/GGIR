1cf5d80f67b57fa263f53be2f25c0f5dcedf83fa
Vincent van Hees <vincentvanhees@gmail.com>
2019-08-09 11:23:53 +0200

    GENEAread does not close connection well when file is corrupt or impossible, now explicit close connections added after each call. Further, replaced cat message to warning or stop messages

--- R/g.readaccfile.R
+++ R/g.readaccfile.R
@@ -106,6 +106,7 @@ g.readaccfile = function(filename,blocksize,blocknumber,selectdaysfile=c(),fileq
         try(expr= {
           P = GENEAread::read.bin(binfile=filename,start=tint[blocknumber,1],
                                   end=tint[blocknumber,2],calibrate=TRUE,do.temp=TRUE,mmap.load=FALSE)
+          on.exit(closeAllConnections())
           if (sf != P$freq) sf = P$freq
         },silent=TRUE)
         if (length(P) <= 2) {
@@ -126,8 +127,9 @@ g.readaccfile = function(filename,blocksize,blocknumber,selectdaysfile=c(),fileq
         if (blocknumber == 1) {
           #try to read without specifying blocks (file too short)
           try(expr={P = GENEAread::read.bin(binfile=filename,start=1,end=10,calibrate=TRUE,do.temp=TRUE,mmap.load=FALSE)},silent=TRUE)
+          on.exit(closeAllConnections())
           if (length(P) == 0) {
-            cat("\nError: file possibly corrupt\n")
+            warning('\nFile possibly corrupt\n')
             P= c(); switchoffLD = 1
             filequality$filecorrupt = TRUE
           } else { #if not then P is now filled with data, but we are not interested in readin this
@@ -154,10 +156,12 @@ g.readaccfile = function(filename,blocksize,blocknumber,selectdaysfile=c(),fileq
       startpage = UPI$startpage;    endpage = UPI$endpage
       try(expr={P = GENEAread::read.bin(binfile=filename,start=startpage,
                                         end=endpage,calibrate=TRUE,do.temp=TRUE,mmap.load=FALSE)},silent=TRUE)
+      on.exit(closeAllConnections())
       if (length(P) <= 2) {
         #try again but now with mmap.load turned on
         try(expr={P = GENEAread::read.bin(binfile=filename,start=startpage,
                                           end=endpage,calibrate=TRUE,do.temp=TRUE,mmap.load=TRUE)},silent=TRUE)
+        on.exit(closeAllConnections())
         if (length(P) != 0) {
           if (sf != P$freq) sf = P$freq
         } else {
@@ -175,8 +179,9 @@ g.readaccfile = function(filename,blocksize,blocknumber,selectdaysfile=c(),fileq
         if (blocknumber == 1) {
           #try to read without specifying blocks (file too short)
           try(expr={P = GENEAread::read.bin(binfile=filename,calibrate=TRUE,do.temp=TRUE,mmap.load=FALSE)},silent=TRUE)
+          on.exit(closeAllConnections())
           if (length(P) == 0) {
-            cat("\nError: file possibly corrupt\n")
+            warning('\nFile possibly corrupt\n')
             P= c(); switchoffLD = 1
             filequality$filecorrupt = TRUE
           } #if not then P is now filled with data
