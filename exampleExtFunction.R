cce8a2bce9258e061298387d6b7456fbe1e4aa9a
vincentvanhees <vincentvanhees@gmail.com>
2019-11-11 15:12:57 +0100

    optional function object now expanded to include model coefficients, #241

--- exampleExtFunction.R
+++ exampleExtFunction.R
@@ -1,5 +1,7 @@
-exampleExtFunction = function(data=c(), sample.rate=c(), datafile="") {
-  output = round(mean(data))
+exampleExtFunction = function(data=c(), model_coefficients=c()) {
+  print(summary(data))
+  print(model_coefficients)
+  output = mean(data) * model_coefficients[1] + mean(data) * model_coefficients[2] + mean(data) * model_coefficients[3]
   print(output)
   return(output)
 }
