c35e09b36111bd9578e423eb738f00b465cc2f93
Vincent van Hees <vincentvanhees@gmail.com>
2019-11-12 14:09:20 +0100

    minor edit

--- exampleExtFunction.R
+++ exampleExtFunction.R
@@ -1,7 +1,7 @@
 exampleExtFunction = function(data=c(), parameters=c()) {
   # parameters: In this function the parameters is just the sample rate.
   library("activityCounts")
-  mycounts = counts(data=data, hertz=parameters,x_axis=1,y_axis=2,z_axis=3)
+  mycounts = counts(data=data, hertz=parameters,x_axis=1,y_axis=2,z_axis=3, start_time = Sys.time())
   mycounts = mycounts[,2:4] #do not provide timestamps to GGIR
   return(mycounts)
 }
