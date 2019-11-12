ed5c35a0e70714b7a52453246592dfc5179a08ff
Vincent van Hees <vincentvanhees@gmail.com>
2019-11-12 09:26:23 +0100

    temp additions to code for development purpose

--- exampleExtFunction.R
+++ exampleExtFunction.R
@@ -1,8 +1,14 @@
-exampleExtFunction = function(data=c(), model_coefficients=c()) {
-  print(summary(data))
+exampleExtFunction = function(data=c(), model_coefficients=c(), sf) {
+  # print(summary(data))
   print(model_coefficients)
   output = mean(data) * model_coefficients[1] + mean(data) * model_coefficients[2] + mean(data) * model_coefficients[3]
   print(output)
   print(dim(data))
+  
+  library("activityCounts")
+  mycounts = counts(data=data, hertz=model_coefficients,x_axis=1,y_axis=2,z_axis=3)
+  print(summary(mycounts))
+  
+  
   return(output)
 }
