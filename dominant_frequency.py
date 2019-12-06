37958f7cf5cf41394a4d0f69fe2674a022b0816a
Vincent van Hees <vincentvanhees@gmail.com>
2019-12-06 17:43:30 +0100

    removing explorative script from package route

--- dominant_frequency.py
+++
@@ -1,8 +0,0 @@
-import numpy
-
-def dominant_frequency(y, sf):
-  fourier = numpy.fft.fft(y)
-  frequencies = numpy.fft.fftfreq(len(y), 1/sf)  # 1/sf is the inter-sample time difference
-  magnitudes = abs(fourier[numpy.where(frequencies > 0)])  # magnitude spectrum
-  peak_frequency = frequencies[numpy.argmax(magnitudes)]
-  return peak_frequency
