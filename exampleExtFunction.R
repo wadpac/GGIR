81688a5b5d87951ddb0161fd46b67fca456d01b8
Vincent van Hees <vincentvanhees@gmail.com>
2019-12-06 17:43:30 +0100

    removing explorative script from package route

--- exampleExtFunction.R
+++
@@ -1,8 +0,0 @@
-exampleExtFunction = function(data=c(), parameters=c()) {
-  # data: matrix where columns are raw acceleration data
-  # parameters: In this function the parameters is just the sample rate.
-  library("activityCounts")
-  mycounts = counts(data=data, hertz=parameters,x_axis=1,y_axis=2,z_axis=3, start_time = Sys.time())
-  mycounts = mycounts[,2:4] #do not provide timestamps to GGIR
-  return(mycounts)
-}
