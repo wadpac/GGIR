2300b5e88218ad40f3ad558e070d915c1515d13f
Vincent van Hees <vincentvanhees@gmail.com>
2020-01-09 17:16:21 +0100

    added new numUnpack function as provided by Dan Jackson

--- R/g.cwaread.R
+++ R/g.cwaread.R
@@ -168,6 +168,42 @@ g.cwaread = function(fileName, start = 0, end = 0, progressBar = FALSE, desiredt
     else
       return(x)
   }
+  
+  numUnpack2 = function(packedData) {
+    #---------------------------------
+    # Code for unpacking provided by Dan Jackson:
+    #---------------------------------
+    count = length(packedData)
+    xyz = integer(count * 3)
+    for (i in 1:count) {
+      # eezzzzzz zzzzyyyy yyyyyyxx xxxxxxxx
+      value = packedData[i]
+      
+      # Isolate each axis component
+      x = bitwAnd(           value     , 0x03ff)
+      y = bitwAnd(bitwShiftR(value, 10), 0x03ff)
+      z = bitwAnd(bitwShiftR(value, 20), 0x03ff)
+      
+      # Signed
+      if (x >= 0x200) { x = x - 0x400 }
+      if (y >= 0x200) { y = y - 0x400 }
+      if (z >= 0x200) { z = z - 0x400 }
+      
+      # Scale
+      shift = bitwAnd(bitwShiftR(value, 30), 0x03)
+      x = bitwShiftL(x, shift)
+      y = bitwShiftL(y, shift)
+      z = bitwShiftL(z, shift)
+      
+      # Store
+      xyz[(i - 1) * 3 + 1] = x
+      xyz[(i - 1) * 3 + 2] = y
+      xyz[(i - 1) * 3 + 3] = z
+    }
+    data = matrix(xyz, ncol=3, byrow=T)
+    data
+  }
+  
   readDataBlock = function(fid, complete = TRUE){
     # Read one block of data and return list with following elements
     #   frequency is frequency recorded in this block
@@ -259,7 +295,9 @@ g.cwaread = function(fileName, start = 0, end = 0, progressBar = FALSE, desiredt
           # Read 4 byte for three measurements
           packedData = readBin(fid, integer(), size = 4, n = blockLength)
           # Unpack data
-          data = numUnpack(packedData)
+          # data = numUnpack(packedData)
+          data = numUnpack2(packedData)
+          
           # Calculate number of bytes to skip
           temp = 482 -  4 *(Naxes/3)* blockLength
         } else {
