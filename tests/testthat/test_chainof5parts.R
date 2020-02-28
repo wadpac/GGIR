32201fa357d619a88dadcdf5c4398fa1cf0047fe
Vincent van Hees <vincentvanhees@gmail.com>
2020-02-28 17:40:04 +0100

    addresses #262 close #262

--- tests/testthat/test_chainof5parts.R
+++ tests/testthat/test_chainof5parts.R
@@ -196,7 +196,7 @@ test_that("chainof5parts", {
   expect_true(dir.exists(dirname))
   expect_true(file.exists(vis_sleep_file))
   expect_that(round(nightsummary$acc_dur_sibd[1],digits=4),equals(0))
-  expect_that(round(nightsummary$acc_SptDuration[1],digits=2),equals(24))
+  expect_that(round(nightsummary$acc_SptDuration[1],digits=2),equals(7))
   expect_true(as.logical(nightsummary$acc_available[1]))
   expect_false(as.logical(nightsummary$sleeplog_used[1]))
   #----------------------------------------------------------------------
