fefa5d99ce5fe42f0626206828229c46e623e85d
Jairo H Migueles <jairo.hidalgo.migueles@gmail.com>
2020-04-13 21:21:01 +0200

    Update g.getmeta.R

--- R/g.getmeta.R
+++ R/g.getmeta.R
@@ -259,6 +259,7 @@ g.getmeta = function(datafile,desiredtz = "",windowsizes = c(5,900,3600),
                         NFilePagesSkipped = filequality$NFilePagesSkipped
                         switchoffLD = accread$switchoffLD
                         PreviousEndPage = accread$endpage
+                        PreviousStartPage = accread$startpage
                         rm(accread); gc()
                         #Temperature read
                         if (mon == 5) { #only for movisens
@@ -279,9 +280,7 @@ g.getmeta = function(datafile,desiredtz = "",windowsizes = c(5,900,3600),
                                 tempRes = matrix(0,nrow = nr, ncol = ncol(rawTemp), dimnames = list(NULL,colnames(rawTemp)))
                                 rawLast = nrow(rawTemp)
                                 tempRes = resample(rawTemp, rawTime, timeRes, rawLast) # this is now the resampled temp data
-                                start_temp = round(PreviousEndPage) - round(blocksize)
-                                end_temp = round(PreviousEndPage) - 1
-                                temperature = tempRes[start_temp:end_temp]
+                                temperature = tempRes[PreviousStartPage:PreviousEndPage]
                                 P = as.data.frame(P)
                                 P[, 4] = temperature
                                 colnames(P[,4]) = "temp"
