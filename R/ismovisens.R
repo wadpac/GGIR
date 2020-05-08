1531d3728325ffe6dc9c559880e1639bcd5c4906
Jairo H Migueles <jairo.hidalgo.migueles@gmail.com>
2020-05-08 20:40:30 +0200

    Update movisens.R
    Update movisens.R to detect movisens files as in the unisensR package

--- R/ismovisens.R
+++ R/ismovisens.R
@@ -1,16 +1,6 @@
-ismovisens = function(fnames){
-        if (TRUE %in% grepl("acc.bin", fnames) & 
-        TRUE %in% grepl("temp.bin", fnames) & 
-        TRUE %in% grepl("angularrate.bin", fnames)) {
-                for (filei in 1:length(fnames)) {
-                        fnames[[filei]] = strsplit(fnames[[filei]], "/")[[1]][1]
-                }
-                fnames = unique(fnames)
-                is.mv = TRUE
-        }
-        else {
-                is.mv = FALSE
-        }
+ismovisens = function(datadir){
+        unisensXML = paste(datadir, "unisens.xml", sep = "/")
+        is.mv = file.exists(unisensXML)
         return(is.mv)
-        }
+}
       
