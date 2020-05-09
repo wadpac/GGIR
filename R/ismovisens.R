1ffdbafe2d3427da4e6ad55961c34ed258001a2b
Jairo H Migueles <jairo.hidalgo.migueles@gmail.com>
2020-05-09 12:45:36 +0200

    Update ismovisens.R to handle directories or files
    Now, it can handle a directory or a file to know if the files stored inside come from a movisens accelerometer

--- R/ismovisens.R
+++ R/ismovisens.R
@@ -1,4 +1,14 @@
 ismovisens = function(datadir){
+        # ---------------------------
+        # In some functions, the file, and not the datadir is introduced in the 
+        # function. Now this allow to handle files or directories
+        isFile = strsplit(datadir, ".", fixed = T)
+        if(length(isFile[[1]]) >= 2) {  
+                datadir_tmp = strsplit(datadir, "/")
+                datadir_ln = length(datadir_tmp[[1]]) - 1
+                datadir = paste(datadir_tmp[[1]][1:datadir_ln], collapse = "/")
+        }
+        # ---------------------------
         unisensXML = paste(datadir, "unisens.xml", sep = "/")
         is.mv = file.exists(unisensXML)
         return(is.mv)
