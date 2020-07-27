20ee5833298841fa593d240c319c8b88eefb2358
Vincent van Hees <vincentvanhees@gmail.com>
2020-07-27 14:33:01 +0200

    rename unit test to be MCS specific

--- tests/testthat/test_MCS_functions.R
+++ tests/testthat/test_MCS_functions.R
@@ -1,9 +1,9 @@
 library(GGIR)
-context("various short functions")
-test_that("various short functions perform well", {
+context("shor functions only used in MCS (Milennium COhort study)")
+test_that("MCS functions still produce same output", {
   
   # These functions were written by Joe Heywood around 2016
-  # and only used for Milenium cohort data
+  # and only used for Milennium cohort data
   # The function include hard coded references to timezone Europe/London 
   # and are not well documented
   # possibly we can remove them from the package at some point
@@ -11,7 +11,7 @@ test_that("various short functions perform well", {
   # For now, it seems good to have at least some unit tests in place:
   
   GAfile  = system.file("testfiles/GENEActiv_testfile.bin", package = "GGIR")[1]
-  d = "30/4/13"
+  d = "30/4/2013"
   options(warn=-1)
   hhr = GENEAread::header.info(binfile = GAfile)
   out = getStartEndNumeric(d, hhr, startHour = 20) 
@@ -26,6 +26,6 @@ test_that("various short functions perform well", {
   
   out3 = getStartEnd(d, startHour = 2)
   expect_equal(nrow(out3), 1)
-  expect_equal(out3[1,2], "01/05/13 02:00:00")
+  expect_equal(out3[1,2], "01/05/2013 02:00:00")
 })
  
