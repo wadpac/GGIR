a9139310fbd11acadcb4e702bf4be8803eedd24b
Vincent van Hees <vincentvanhees@gmail.com>
2020-08-12 12:26:59 +0200

    definedays now better accounts for measurements that start at midnight when window is MM

--- R/g.part5.definedays.R
+++ R/g.part5.definedays.R
@@ -8,41 +8,46 @@ g.part5.definedays = function(nightsi, wi, summarysleep_tmp2, indjump, nightsi_b
   # Check that it is possible to find both windows (WW and MM)
   # in the data for this day.
   if (timewindowi == "MM") {
-    if (wi==1) {
-      qqq[1] = 1
-      qqq[2] = nightsi[wi]
-    } else if (wi<=nrow(summarysleep_tmp2)) {
-      qqq[1] = nightsi[wi-1] + 1
-      qqq[2] = nightsi[wi]
-      qqq_backup = qqq
-    } else if (wi>nrow(summarysleep_tmp2)) {
-      qqq[1] = qqq_backup[2] + 1
-      if (wi <= length(nightsi)) {
+    if (nightsi[1] == 1) wi = wi + 1 # if recording starts at midnight
+    if (length(nightsi) >= wi) {
+      if (wi==1) {
+        qqq[1] = 1
         qqq[2] = nightsi[wi]
-      } else {
-        if (wi-indjump <= length(nightsi)) {
-          tmp1 = which(nightsi_bu == nightsi[wi-indjump])
-          qqq[2] = nightsi_bu[tmp1 + indjump]
-          indjump = indjump + 1 # in case there are multiple days beyond nightsi
-          if (is.na(qqq[2])) { # if that does not work use last midnight and add 24 hours
-            index_lastmidn = which(nightsi_bu == nightsi[wi-(indjump-1)]) + (indjump-1)
-            if (length(index_lastmidn) > 0) {
-              qqq[2] = nightsi_bu[index_lastmidn] + (24*(60/ws3new) * 60) -1
-            } else {
-              qqq[2] = NA
-            }
-          }
+      } else if (wi<=nrow(summarysleep_tmp2)) {
+        qqq[1] = nightsi[wi-1] + 1
+        qqq[2] = nightsi[wi]
+        qqq_backup = qqq
+      } else if (wi>nrow(summarysleep_tmp2)) {
+        qqq[1] = qqq_backup[2] + 1
+        if (wi <= length(nightsi)) {
+          qqq[2] = nightsi[wi]
         } else {
-          qqq[2] = NA
+          if (wi-indjump <= length(nightsi)) {
+            tmp1 = which(nightsi_bu == nightsi[wi-indjump])
+            qqq[2] = nightsi_bu[tmp1 + indjump]
+            indjump = indjump + 1 # in case there are multiple days beyond nightsi
+            if (is.na(qqq[2])) { # if that does not work use last midnight and add 24 hours
+              index_lastmidn = which(nightsi_bu == nightsi[wi-(indjump-1)]) + (indjump-1)
+              if (length(index_lastmidn) > 0) {
+                qqq[2] = nightsi_bu[index_lastmidn] + (24*(60/ws3new) * 60) -1
+              } else {
+                qqq[2] = NA
+              }
+            }
+          } else {
+            qqq[2] = NA
+          }
+          if (is.na(qqq[2])) { # if that does not work use last midnight and add 24 hours
+            qqq[2] = qqq_backup[2] + (24*(60/ws3new) * 60) -1
+          }
+          if (qqq[1] == qqq[2])  qqq[2] = qqq[2] + (24*(60/ws3new) * 60) - 1
         }
-        if (is.na(qqq[2])) { # if that does not work use last midnight and add 24 hours
-          qqq[2] = qqq_backup[2] + (24*(60/ws3new) * 60) -1
+        if(is.na(qqq[2])==TRUE | Nts < qqq[2]) {
+          qqq[2] = Nts
         }
-        if (qqq[1] == qqq[2])  qqq[2] = qqq[2] + (24*(60/ws3new) * 60) - 1
       }
-      if(is.na(qqq[2])==TRUE | Nts < qqq[2]) {
-        qqq[2] = Nts
-      }
+    } else {
+      qqq = c(NA,NA)
     }
   } else if(timewindowi == "WW") {
     if (wi <=(Nwindows-1)) { # all full wake to wake days
