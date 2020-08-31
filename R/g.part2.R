e6c4467fba9867f04c0fc4fd8a5df37d5b641884
Vincent van Hees <vincentvanhees@gmail.com>
2020-08-31 09:48:58 +0200

    undo testing edits

--- R/g.part2.R
+++ R/g.part2.R
@@ -92,12 +92,11 @@ g.part2 = function(datadir=c(),metadatadir=c(),f0=c(),f1=c(),strategy = 1, hrs.d
   fe_dopar = foreach::`%dopar%`
   fe_do = foreach::`%do%`
   i = 0 # declare i because foreach uses it, without declaring it
-  # `%myinfix%` = ifelse(do.parallel, fe_dopar, fe_do) # thanks to https://stackoverflow.com/questions/43733271/how-to-switch-programmatically-between-do-and-dopar-in-foreach
-  # output_list =foreach::foreach(i=f0:f1, .packages = packages2passon,
-  #                               .export=functions2passon, .errorhandling=errhand, .verbose = F) %myinfix% { # the process can take easily 1 minute per file, so probably there is a time gain by doing it parallel
-  #   tryCatchResult = tryCatch({
-  for (i in f0:f1) {
-    print(".")
+  `%myinfix%` = ifelse(do.parallel, fe_dopar, fe_do) # thanks to https://stackoverflow.com/questions/43733271/how-to-switch-programmatically-between-do-and-dopar-in-foreach
+  output_list =foreach::foreach(i=f0:f1, .packages = packages2passon,
+                                .export=functions2passon, .errorhandling=errhand, .verbose = F) %myinfix% { # the process can take easily 1 minute per file, so probably there is a time gain by doing it parallel
+    tryCatchResult = tryCatch({
+  # for (i in f0:f1) {
     if (length(ffdone) > 0) {
       if (length(which(ffdone == as.character(unlist(strsplit(fnames[i],"eta_"))[2]))) > 0) {
         skip = 1 #skip this file because it was analysed before")
@@ -211,10 +210,10 @@ g.part2 = function(datadir=c(),metadatadir=c(),f0=c(),f1=c(),strategy = 1, hrs.d
       if (M$filecorrupt == FALSE & M$filetooshort == FALSE) rm(IMP)
       rm(M); rm(I)
     }
-  } # end for loopp
-  #   }) # END tryCatch
-  #   return(tryCatchResult)
-  # }
+  # } # end for loopp
+    }) # END tryCatch
+    return(tryCatchResult)
+  }
   if (do.parallel == TRUE) {
     on.exit(parallel::stopCluster(cl))
     for (oli in 1:length(output_list)) { # logged error and warning messages
