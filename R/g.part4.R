7a5394420a7703feb2968027a6645230c8fee8b0
Vincent van Hees <vincentvanhees@gmail.com>
2020-09-07 14:16:45 +0200

    change default coln1 to 2, because it makes no sense that this is 9

--- R/g.part4.R
+++ R/g.part4.R
@@ -1,5 +1,5 @@
 g.part4 = function(datadir=c(),metadatadir=c(),f0=f0,f1=f1,idloc=1,loglocation = c(),
-                   colid = 1,coln1 = 9,nnights = 7,sleeplogidnum=FALSE,do.visual=FALSE,outliers.only = FALSE,
+                   colid = 1,coln1 = 2,nnights = 7,sleeplogidnum=FALSE,do.visual=FALSE,outliers.only = FALSE,
                    excludefirstlast=FALSE,criterror = 1,includenightcrit=16,
                    relyonguider=FALSE,relyonsleeplog=FALSE, def.noc.sleep=1,
                    storefolderstructure=FALSE,
