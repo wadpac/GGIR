1ac35219eb3422f97bd7d2b6209f55646cf92ae2
Vincent van Hees <vincentvanhees@gmail.com>
2021-04-13 16:57:24 +0200

    improved check for providing depricated argument IVIS_epochsize_seconds

--- R/g.IVIS.R
+++ R/g.IVIS.R
@@ -1,6 +1,8 @@
 g.IVIS = function(Xi, epochsizesecondsXi = 5, IVIS_epochsize_seconds=NA, IVIS_windowsize_minutes = 60, IVIS.activity.metric = 1) {
-  if (!is.na(IVIS_epochsize_seconds)) {
-    warning("Argument IVIS_epochsize_seconds has been depricated")
+  if (length(IVIS_epochsize_seconds) > 0) {
+    if (!is.na(IVIS_epochsize_seconds)) {
+      warning("Argument IVIS_epochsize_seconds has been depricated")
+    }
   }
   IVIS_epochsize_seconds = IVIS_windowsize_minutes*60
   if (IVIS.activity.metric == 2) { # use binary scoring
