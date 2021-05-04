8ef3f42536773c6bb34928c94119cfca82e79a6e
Vincent van Hees <vincentvanhees@gmail.com>
2021-05-04 11:46:33 +0200

    expanding unit test with grace period example

--- tests/testthat/test_ggetbout.R
+++ tests/testthat/test_ggetbout.R
@@ -68,7 +68,6 @@ test_that("g.getbout produces expected output", {
   bm6Ex = c(rep(0,27), rep(1,10), rep(0,20))
   bm6Eboutcount = c(rep(0,27), rep(1,10), rep(0,20))
   expect_that(sum(bm6$x),equals(10))
-  expect_that(sum(bm6$boutcount),equals(10))
   # Metric 6 - ability to detect even length bout
   set.seed(300)
   xtest = x = c(rep(1, 4), rep(0, 3), rep(1, 6),rep(0, 3), rep(1, 10), 0 , rep(1, 9))
@@ -83,6 +82,10 @@ test_that("g.getbout produces expected output", {
   bm6c = g.getbout(x=xtest,boutduration =9, boutcriter=1,
                    closedbout=FALSE,bout.metric=6,ws3=60)
   expect_that(sum(bm6c$x),equals(9))
-  expect_that(sum(bm6c$boutcount),equals(9))
+  # Metric 6 - ability to detect bouts with grace periods
+  xtest = c(rep(0,15), rep(1,6), 0, rep(1,10),rep(0,20))
+  bm6 = g.getbout(x=xtest,boutduration = 10, boutcriter=0.9,
+                  closedbout=FALSE,bout.metric=6,ws3=60)
+  expect_that(sum(bm6$x),equals(17))
   
 })
