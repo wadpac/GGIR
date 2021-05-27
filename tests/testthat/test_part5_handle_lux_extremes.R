032445ffbe29325d7805f07676fc67c257f50fc9
Vincent van Hees <vincentvanhees@gmail.com>
2021-05-27 22:20:11 +0200

    adding skiponcran statement in some unit tests

--- tests/testthat/test_part5_handle_lux_extremes.R
+++ tests/testthat/test_part5_handle_lux_extremes.R
@@ -1,6 +1,7 @@
 library(GGIR)
 context("g.part5.handle_lux_extremes")
 test_that("is able to handle lux extremes", {
+  skip_on_cran()
   lux = c(130000, 1, 1, 1, 130000, 1, 1, 1, 130000, 130000, 1, 1, 1, 130000, 130000, 130000, 1, 1, 130000)
   out = g.part5.handle_lux_extremes(lux)
   expect_equal(length(which(out$correction_log == 2)), 3)
