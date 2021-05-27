f37f22badc383c14252d3359d143277ab1d09b6a
Vincent van Hees <vincentvanhees@gmail.com>
2021-05-27 21:25:15 +0200

    unit test for handling extreme lux values expanded to also cover absense of extreme values

--- tests/testthat/test_part5_handle_lux_extremes.R
+++ tests/testthat/test_part5_handle_lux_extremes.R
@@ -6,4 +6,10 @@ test_that("is able to handle lux extremes", {
   expect_equal(length(which(out$correction_log == 2)), 3)
   expect_equal(length(which(out$correction_log == 1)), 5)
   expect_equal(mean(out$lux, na.rm=TRUE), 1)
+  
+  # test also absense of extreme values
+  lux = rep(1, 12)
+  out = g.part5.handle_lux_extremes(lux)
+  expect_equal(sum(out$lux), 12)
+  expect_equal(sum(out$correction_log), 0)
 })
