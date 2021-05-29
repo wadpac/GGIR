159c4be862f2eb8715af552677cc8417d7d95037
Vincent van Hees <vincentvanhees@gmail.com>
2021-05-29 18:46:37 +0200

    reactivating and updating unittest for gloadlog now gloadlog extracts startdate of recordings itself

--- tests/testthat/test_gloadlog.R
+++ tests/testthat/test_gloadlog.R
@@ -4,14 +4,18 @@ test_that("gloadlog is able to load different log formats", {
   create_test_sleeplog_csv(Nnights=7, advanced=TRUE)
   fn = "testsleeplogfile.csv"
   expect_true(file.exists(fn))
-  # TO DO: This test needs to be re-writen now g.loadlog function has changed
-  # startdates = data.frame(ID = "123", startdate = "2015-03-23")
-  # logs = g.loadlog(loglocation=fn,coln1=2,colid=1,nnights=7,sleeplogidnum=TRUE,startdates = startdates, sleeplogsep=",")
-  # expect_equal(nrow(logs$sleeplog), 7)
-  # expect_equal(ncol(logs$sleeplog), 5)
-  # expect_equal(nrow(logs$nonwearlog), 7)
-  # expect_equal(ncol(logs$nonwearlog), 4)
-  # expect_equal(nrow(logs$naplog), 7)
-  # expect_equal(ncol(logs$naplog), 6)
-  # if (file.exists(fn)) file.remove(fn)
+  tempdir = "mytestdir"
+  if (dir.exists(tempdir) == FALSE) dir.create(tempdir)
+  ID = "123A"
+  rec_starttime = "2016-06-25T20:20:20+0200"
+  save(ID, rec_starttime, file = "mytestdir/dummyms3.RData")
+  logs = g.loadlog(loglocation=fn, coln1=2, colid=1, nnights=7, sleeplogidnum=FALSE,
+                   sleeplogsep=",", meta.sleep.folder=tempdir , desiredtz = "")
+  expect_equal(nrow(logs$sleeplog), 7)
+  expect_equal(ncol(logs$sleeplog), 5)
+  expect_equal(nrow(logs$nonwearlog), 7)
+  expect_equal(ncol(logs$nonwearlog), 4)
+  expect_equal(nrow(logs$naplog), 7)
+  expect_equal(ncol(logs$naplog), 6)
+  if (dir.exists(tempdir)) unlink(tempdir, recursive=TRUE)
 })
