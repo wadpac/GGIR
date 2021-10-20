8d113d61362cdc39decb223fe3361fffdd0295f3
Vincent van Hees <vincentvanhees@gmail.com>
2021-10-20 11:28:17 +0200

    improving documentation on Galland and Sadeh algorithm and tidying up code a bit

--- R/HASIB.R
+++ R/HASIB.R
@@ -1,31 +1,30 @@
-HASIB = function(HASIB.algo = "vanHees2015", timethreshold=c(), anglethreshold=c(), 
-                 time=c(), anglez=c(), ws3=c(), zeroCrossingCount=c()) {
-  
-  sumperminutes = function(x, ws3) {
-    x2 =cumsum(c(0,x))
-    select = seq(1,length(x2),by=60/ws3)
+HASIB = function(HASIB.algo = "vanHees2015", timethreshold = c(), anglethreshold = c(), 
+                 time = c(), anglez = c(), ws3 = c(), zeroCrossingCount = c()) {
+  epochsize = ws3 #epochsize in seconds
+  sumPerMinute = function(x, epochsize) {
+    x2 = cumsum(c(0, x))
+    select = seq(1, length(x2), by = 60/epochsize)
     x3 = diff(x2[select])
   }
-  
   create_rollfun_mat = function(x, Ncol) {
     Nz = length(x)
     # Generate matrix to ease applying rolling function
-    x_matrix = matrix(0, Nz+(Ncol-1), Ncol)
+    x_matrix = matrix(0, Nz + (Ncol - 1), Ncol)
     for (jj in 1:Ncol) {
-      x_matrix[jj:(Nz+jj-1), jj] = x
+      x_matrix[jj:(Nz + jj - 1), jj] = x
       if (jj > 1) {
-        x_matrix[1:(jj-1), jj] = x[1]
+        x_matrix[1:(jj - 1), jj] = x[1]
       }
       if (jj < Ncol) {
-        x_matrix[((Nz-((Ncol-1)-jj)):Nz)+(Ncol-1), jj] = tail(x, 1)
+        x_matrix[((Nz - ((Ncol - 1) - jj)):Nz) + (Ncol - 1), jj] = tail(x, 1)
       }
     }
     return(x_matrix)
   }
   
-  reformat_output = function(x, time, ws3) {
+  reformat_output = function(x, time, epochsize) {
     # resample to original resolution
-    x = rep(x, each=(60/ws3))
+    x = rep(x, each = (60/epochsize))
     # resize to match length of time
     if (length(x) < length(time)) {
       x = c(x,rep(0, length(time) - length(x)))
@@ -50,11 +49,11 @@ HASIB = function(HASIB.algo = "vanHees2015", timethreshold=c(), anglethreshold=c
         # count posture changes that happen less than once per ten minutes
         q1 = c()
         if (length(postch) > 1) {
-          q1 = which(diff(postch) > (i*(60/ws3))) #less than once per i minutes
+          q1 = which(diff(postch) > (i*(60/epochsize))) #less than once per i minutes
         }
         if (length(q1) > 0) {
           for (gi in 1:length(q1)) {
-            sdl1[postch[q1[gi]]:postch[q1[gi]+1]] = 1 #periods with no posture change
+            sdl1[postch[q1[gi]]:postch[q1[gi] + 1]] = 1 #periods with no posture change
           }
         } else { #possibly a day without wearing
           if (length(postch) < 10) {  #possibly a day without wearing
@@ -64,32 +63,32 @@ HASIB = function(HASIB.algo = "vanHees2015", timethreshold=c(), anglethreshold=c
           }
         }
         sib_classification[,cnt] = sdl1
-        cnt = cnt+ 1
+        cnt = cnt + 1
       }
     }
     cnt = 1
     sib_classification = as.data.frame(sib_classification, stringsAsFactors = TRUE)
     for (i in timethreshold) {
       for (j in anglethreshold) {
-        colnames(sib_classification)[cnt] = paste("T",i,"A",j,sep="")
+        colnames(sib_classification)[cnt] = paste("T", i, "A", j, sep = "")
         cnt = cnt + 1
       }
     }
   } else if (HASIB.algo == "Sadeh1994") {
-    ZCpermin = sumperminutes(zeroCrossingCount, ws3=ws3)
+    ZCpermin = sumPerMinute(zeroCrossingCount, epochsize = epochsize)
     
-    ZCpermin_matrix = create_rollfun_mat(ZCpermin, Ncol=11)
+    ZCpermin_matrix = create_rollfun_mat(ZCpermin, Ncol = 11)
     CalcSadehFT = function(x) {
-      MeanW5 = mean(x, na.rm=TRUE)
+      MeanW5 = mean(x, na.rm = TRUE)
       SDlast = sd(x[6:11]) #last five in this matrix means columns 6:11
       NAT = length(which(x > 50 & x < 100))
-      LOGact = log(x[6]+1)
-      return(data.frame(MeanW5=MeanW5, SDlast=SDlast,
-                        NAT=NAT, LOGact=LOGact))
+      LOGact = log(x[6] + 1)
+      return(data.frame(MeanW5 = MeanW5, SDlast = SDlast,
+                        NAT = NAT, LOGact = LOGact))
     }
     SadehFT1 = apply(X = ZCpermin_matrix, MARGIN = 1, FUN = CalcSadehFT)
     rm(ZCpermin_matrix)
-    SadehFT = data.frame(matrix(unlist(SadehFT1), nrow=length(SadehFT1), byrow=TRUE))
+    SadehFT = data.frame(matrix(unlist(SadehFT1), nrow = length(SadehFT1), byrow = TRUE))
     rm(SadehFT1)
     colnames(SadehFT) = c("MeanW5", "SDlast", "NAT", "LOGact")
     # apply Sadeh algorithm
@@ -100,19 +99,21 @@ HASIB = function(HASIB.algo = "vanHees2015", timethreshold=c(), anglethreshold=c
       PSscores[PSsibs] = 1
     }
     # resample to original resolution and ensure length matches length of time
-    sib_classification = reformat_output(x=PSscores, time, ws3)
+    sib_classification = reformat_output(x = PSscores, time, epochsize)
     colnames(sib_classification)[1] = HASIB.algo
   } else if (HASIB.algo == "Galland2012") {  
     # Aggregate per minute
-    ZCpermin = sumperminutes(zeroCrossingCount, ws3=ws3)
+    ZCpermin = sumPerMinute(zeroCrossingCount, epochsize = epochsize)
     mean_nonzero = mean(ZCpermin[which(ZCpermin != 0)])
     CountScaled = ZCpermin / mean_nonzero
-    CountScaled_matrix = create_rollfun_mat(CountScaled, Ncol=7)
-    WeightCounts = abs(rowSums(CountScaled_matrix * c(1,3,5:1))) * 2.7 # intentional reversed order
+    CountScaled_matrix = create_rollfun_mat(CountScaled, Ncol = 7)
+    # In next line I use intentionally a reversed order relative to Galland publication
+    # because our matrix is reversed relative to paper
+    WeightCounts = abs(rowSums(CountScaled_matrix * c(1,3,5:1))) * 2.7 
     WeightCounts = WeightCounts[-c(1:2)] # remove first two time stamps, to align with 5th element (7-5=2)
     GallandScore = rep(0, length(WeightCounts))
     GallandScore[which(WeightCounts < 1)] = 1
-    sib_classification = reformat_output(x=GallandScore, time, ws3)
+    sib_classification = reformat_output(x = GallandScore, time, epochsize)
     colnames(sib_classification)[1] = HASIB.algo
   }
   return(sib_classification)
