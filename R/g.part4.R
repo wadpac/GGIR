d2a4e5aa48623830eb009c6bfde8d8af6bc6d713
Vincent van Hees <v.vanhees@accelting.com>
2022-01-24 16:19:54 +0100

    fix to #496

--- R/g.part4.R
+++ R/g.part4.R
@@ -525,18 +525,20 @@ g.part4 = function(datadir = c(), metadatadir = c(), f0 = f0, f1 = f1,
                     relyonguider_thisnight = TRUE
                   }
                 }
+                if (length(spo) == 0) {
+                  # add empty spo object, in case it was removed above
+                  # we do this because code below assumes that spo is a matrix
+                  spo = matrix(0, nsp, 5)
+                  spo[1, 1] = 1
+                  spo[1, 2:4] = 0
+                  spo[1, 5] = k
+                } 
                 # If no SIBs overlap with the SPT window
                 if (length(which(spo[, 2] < SptWake & 
                                  spo[, 3] > SptOnset)) == 0 | 
                     relyonguider_thisnight == TRUE) {
                   # If night is explicitely listed
                   cleaningcode = 5
-                  if (length(spo) == 0) { # add empty spo object, in case it was removed above
-                    spo = matrix(0, nsp, 5)
-                    spo[1, 1] = 1
-                    spo[1, 2:4] = 0
-                    spo[1, 5] = k
-                  } 
                   newlines = rbind(spo[1, ], spo[1, ])
                   newlines[1, 1:4] = c(nrow(spo) + 1, SptOnset, SptOnset + 1/60, 1)
                   newlines[2, 1:4] = c(nrow(spo) + 1, SptWake - 1/60, SptWake, 1)
