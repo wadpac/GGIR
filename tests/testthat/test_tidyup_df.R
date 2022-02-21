129a0e7b9a42e85dac01f989559c998b25ec32db
Jairo H Migueles <jairo.hidalgo.migueles@gmail.com>
2022-02-21 16:35:56 +0100

    Ammend tidyup test file.

--- tests/testthat/test_tidyup_df.R
+++ tests/testthat/test_tidyup_df.R
@@ -6,7 +6,7 @@ test_that("tidyup_df returns a data frame with the numeric columns rounded", {
                   factor = factor(c("f1", "f2")), missing = c(NA, "NaN"))
   df_converted = tidyup_df(df = df, digits = 3)
   expect_equal(df_converted$letters, df$letters)
-  expect_equal(df_converted$nums, round(df$nums, 3))
+  expect_equal(df_converted$nums, c(1.543, 8.217))
   expect_equal(df_converted$factor, df$factor)
   expect_equal(df_converted$missing, c("", ""))
 })
