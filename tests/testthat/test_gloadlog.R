891820b6745ce3df2c5dafcb0908441b5a701b6f
Vincent van Hees <vincentvanhees@gmail.com>
2022-04-12 11:34:01 +0200

    expand unit test to als cover scenario where start of acc recording is much earlier than start of sleeplog

--- tests/testthat/test_gloadlog.R
+++ tests/testthat/test_gloadlog.R
@@ -9,13 +9,26 @@ test_that("gloadlog is able to load different log formats", {
   ID = "123A"
   rec_starttime = "2016-06-25T20:20:20+0200"
   save(ID, rec_starttime, file = "mytestdir/dummyms3.RData")
-  logs = g.loadlog(loglocation = fn, coln1 = 2, colid = 1, nnights = 7, sleeplogidnum = FALSE,
+  logs1 = g.loadlog(loglocation = fn, coln1 = 2, colid = 1, nnights = 7, sleeplogidnum = FALSE,
                    sleeplogsep = ",", meta.sleep.folder = tempdir , desiredtz = "")
-  expect_equal(nrow(logs$sleeplog), 7)
-  expect_equal(ncol(logs$sleeplog), 5)
-  expect_equal(nrow(logs$nonwearlog), 7)
-  expect_equal(ncol(logs$nonwearlog), 4)
-  expect_equal(nrow(logs$naplog), 7)
-  expect_equal(ncol(logs$naplog), 6)
+  expect_equal(nrow(logs1$sleeplog), 7)
+  expect_equal(ncol(logs1$sleeplog), 5)
+  expect_equal(logs1$sleeplog$night, as.character(1:7))
+  expect_equal(nrow(logs1$nonwearlog), 7)
+  expect_equal(ncol(logs1$nonwearlog), 4)
+  expect_equal(nrow(logs1$naplog), 7)
+  expect_equal(ncol(logs1$naplog), 6)
+  # start of accelerometer recording 6 days earlier
+  rec_starttime = "2016-06-20T20:20:20+0200"
+  save(ID, rec_starttime, file = "mytestdir/dummyms3.RData")
+  logs2 = g.loadlog(loglocation = fn, coln1 = 2, colid = 1, nnights = 7, sleeplogidnum = FALSE,
+                   sleeplogsep = ",", meta.sleep.folder = tempdir , desiredtz = "")
+  expect_equal(nrow(logs2$sleeplog), 7)
+  expect_equal(ncol(logs2$sleeplog), 5)
+  expect_equal(logs2$sleeplog$night, as.character(6:12))
+  expect_equal(nrow(logs2$nonwearlog), 7)
+  expect_equal(ncol(logs2$nonwearlog), 4)
+  expect_equal(nrow(logs2$naplog), 7)
+  expect_equal(ncol(logs2$naplog), 6)
   if (dir.exists(tempdir)) unlink(tempdir, recursive = TRUE)
 })
