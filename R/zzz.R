3f5ab1c79a1f301079ffd91448147ee0afa127a0
Vincent van Hees <v.vanhees@accelting.com>
2022-06-03 17:44:11 +0200

    addressing #575

---
+++ R/zzz.R
@@ -0,0 +1,13 @@
+.onAttach <- function(...) {
+  if (!interactive()) return()
+  pkgs <- available.packages()
+  cran_version <- package_version(pkgs[which(pkgs[,1] == "GGIR"),"Version"])
+  local_version <- packageVersion("GGIR")
+  behind_cran <- cran_version > local_version
+  if (interactive()) {
+    if (behind_cran) {
+      msg <- paste0("Did you know that a newer version of GGIR (", cran_version,") is available with bug fixes and new features? Do not forget to install it.")
+      packageStartupMessage(msg)
+    }   
+  }
+}
