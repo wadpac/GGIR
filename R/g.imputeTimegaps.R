9051a07fcd3a28da2753de05340a30b3da90c6a7
Vincent van Hees <v.vanhees@accelting.com>
2022-08-09 18:01:39 +0200

    undoing all the work in this branch in relation to timegap imputation because it seems to overcomplicating the work

--- R/g.imputeTimegaps.R
+++ R/g.imputeTimegaps.R
@@ -1,4 +1,4 @@
-g.imputeTimegaps = function(x, xyzCol, timeCol = c(), sf, impute = TRUE) {
+g.imputeTimegaps = function(x, xyzCol, timeCol = c(), sf, k=0.25, impute = TRUE) {
   remove_time_at_end = FALSE
   if (length(timeCol) == 0) { # add temporary timecolumn to enable timegap imputation where there are zeros
     dummytime = Sys.time()
@@ -11,7 +11,6 @@ g.imputeTimegaps = function(x, xyzCol, timeCol = c(), sf, impute = TRUE) {
     timeCol = "time"
     remove_time_at_end = TRUE
   }
-
   zeros = which(rowSums(x[,xyzCol]) == 0)
   if (length(zeros) > 0) {
     if (zeros[1] == 1) {
@@ -21,26 +20,20 @@ g.imputeTimegaps = function(x, xyzCol, timeCol = c(), sf, impute = TRUE) {
     x = x[-zeros,]
   }
   if (isTRUE(impute)) { # this is default, in g.calibrate this is set to FALSE
-    # prevent trying to impute timegaps shorter than 5 seconds
-    k = 5
-    x$time_utc = as.POSIXct(x = as.numeric(x$time), tz = "UTC", origin = "1970-1-1")
-    deltatime = diff(x[, "time_utc"]) #timeCol
+    if (k < 2/sf) { # prevent trying to impute timegaps shorter than 2 samples
+      k = 2/sf
+    }
+    deltatime = diff(x[, timeCol])
     if (!is.numeric(deltatime)) {  # in csv axivity, the time is directly read as numeric (seconds)
       units(deltatime) = "secs"
       deltatime = as.numeric(deltatime)
     }
-    gapsi = which(deltatime >= k) # limit imputation to gaps larger than 5 seconds
+    gapsi = which(deltatime >= k) # limit imputation to gaps larger than 0.25 seconds
     NumberOfGaps = length(gapsi)
-    if (sum(deltatime[gapsi]) > 3600 * 36) {
-      stop(paste0("There is more than 36 hours of missing data to be imputed.",
-                  " GGIR can currently not handle this. As a temporary solution: ", 
-                  "If you are using ActiGraph gt3x data export the data to csv ",
-                  "with the ActiLife software."))
-    }
     if (NumberOfGaps > 0) { 
       # if gaps exist impute them by repeating the last known value
       x$gap = 1
-      x$gap[gapsi] = as.integer(deltatime[gapsi] * sf) # convert to samples, because we impute samples not seconds
+      x$gap[gapsi] = as.integer(deltatime[gapsi] * sf) 
       x <- as.data.frame(lapply(x, rep, x$gap))
       #  normalise last known value to 1
       i_normalise = which(x$gap != 1)
@@ -49,7 +42,6 @@ g.imputeTimegaps = function(x, xyzCol, timeCol = c(), sf, impute = TRUE) {
       }
       x = x[, which(colnames(x) != "gap")]
     }
-    x = x[,-which(colnames(x) == "time_utc")]
   }
   # Note: Timestamps are not imputed because from here onward GGIR does not need them
   # Any problems with sample rate should have been fixed during data loading
