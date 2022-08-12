6689d6be51e9484470a1cbab520cc02858bd15a9
Vincent van Hees <v.vanhees@accelting.com>
2022-08-12 17:02:37 +0200

    replace search for duplicated by search for identical succeeding values

--- R/g.calibrate.R
+++ R/g.calibrate.R
@@ -335,7 +335,7 @@ g.calibrate = function(datafile, params_rawdata = c(),
                            abs(as.numeric(meta_temp[,2])) < 2 & abs(as.numeric(meta_temp[,3])) < 2 &
                            abs(as.numeric(meta_temp[,4])) < 2) #the latter three are to reduce chance of including clipping periods
       meta_temp = meta_temp[nomovement,]
-      dup = which(duplicated(meta_temp[,2]) & duplicated(meta_temp[,3]) & duplicated(meta_temp[,4])) # remove duplicated values
+      dup = which(rowSums(meta_temp[1:(nrow(meta_temp) - 1), 2:7] == meta_temp[2:nrow(meta_temp), 2:7]) == 3) # remove duplicated values
       if (length(dup) > 0) meta_temp = meta_temp[-dup,]
       rm(nomovement, dup)
       if (min(dim(meta_temp)) > 1) {
