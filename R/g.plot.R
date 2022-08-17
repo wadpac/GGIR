6a8f0e6a0de212de74ec800464bad9b0b93591bd
Vincent van Hees <v.vanhees@accelting.com>
2022-08-17 14:57:24 +0200

    address #615

--- R/g.plot.R
+++ R/g.plot.R
@@ -51,91 +51,120 @@ g.plot = function(IMP, M, I, durplot) {
   
   # start plot with empty canvas
   plot.new()	
-  par(fig = c(0, 1, 0, 1), new = T)
+  par(fig = c(0, 1, 0, 1), new = T, mar = c(5, 4, 3, 0))
   plot(seq(0, durplot), seq(0, durplot), col = "white", type = "l", axes = F, 
-       xlab = "", ylab = "", main = paste0("device: ", monn, " | filename: ", fname), cex.main = 0.6)#dummy plot
+       xlab = "", ylab = "", main = paste0("device brand: ", monn, " | filename: ", fname), cex.main = 0.6)#dummy plot
   # lim = par("usr")
   # draw coloured rectangles
   Y0 = -50
-  Y1 = c(durplot + n_ws2_perday)
+  Y1 = c(durplot) # + n_ws2_perday)
+  legend_names = c("non-wear", "signal clipping", "more non-wear", "study protocol")
+  legend_lty = c(NA, NA, NA, NA)
+  legend_density = c(100, 100, 100, dens)
+  x.intersp = rep(0.5, 4)
+  legend_colors = c()
+  legend_index = c()
   if (length(s0) > 0) { #non-wear
+    CL = colors()[148]
     for (ri in 1:length(s0)) {
-      rect(s0[ri], Y0, s1[ri], Y1, border = colors()[148], col = colors()[148]) #red 404
+      rect(s0[ri], Y0, s1[ri], Y1, border = colors()[148], col = CL) #red 404
     }
+    legend_index = 1
+    legend_colors = CL
   }
   if (length(b0) > 0) { #clip
+    CL = colors()[464]
     for (ri in 1:length(b0)) {
-      rect(b0[ri], Y0, b1[ri], Y1, border = colors()[464], col = colors()[464])
+      rect(b0[ri], Y0, b1[ri], Y1, border = colors()[464], col = CL)
     }
+    legend_index = c(legend_index, 2)
+    legend_colors = c(legend_colors, CL)
   }
   if (length(g0) > 0) {
-    for (ri in 1:length(g0)) { #end blue
-      rect(g0[ri], Y0, g1[ri], Y1, border = colors()[150], col = colors()[150])
+    CL = colors()[150]
+    for (ri in 1:length(g0)) { #additional non-wear
+      rect(g0[ri], Y0, g1[ri], Y1, border = colors()[150], col = CL)
     }
+    legend_index = c(legend_index, 3)
+    legend_colors = c(legend_colors, CL)
   }
   if (length(w0) > 0) {
-    for (ri in 1:length(w0)) { #end blue
-      rect(w0[ri], Y0, w1[ri], Y1, border = colors()[24], col = colors()[24], density = dens)
+    CL = colors()[24]
+    for (ri in 1:length(w0)) { #protocol
+      rect(w0[ri], Y0, w1[ri], Y1, border = colors()[24], col = CL, density = dens)
     }
+    legend_index = c(legend_index, 4)
+    legend_colors = c(legend_colors, CL)
   }
+  
+  # Add legend for rectengles
+  legend_names = legend_names[legend_index]
+  legend_lty = legend_lty[legend_index]
+  legend_density = legend_density[legend_index]
+  x.intersp = x.intersp[legend_index]
+  
+  if (length(legend_index) > 0) {
+    legend("top", legend = legend_names, col = legend_colors, density = legend_density, #lty = legend_lty,  lty = legend_lty, 
+           fill = legend_colors, border = legend_colors, x.intersp = x.intersp, ncol = 4, cex = 0.7, bty = 'n')
+  }
+  
   abline(v = timeline[length(timeline)], col = "blue", lwd = 1)	
+  
   # derive acceleration signal
-  if (length(which(colnames(M$metashort) == "ENMO")) > 0) {
-    accel = M$metashort$ENMO
-  } else {
-    # Index of first acceleration metric in metashort other than ENMO, timestamp, or angle-related
-    IndOtEnmo = which(colnames(M$metashort) %in% c("ENMO","timestamp","anglex","angley","anglez") == FALSE)[1]
-    accel = as.numeric(as.matrix(M$metashort[,IndOtEnmo]))
-  }
+  # Index of first acceleration metric in metashort other than timestamp, or angle-related
+  IndOtMetric = which(colnames(M$metashort) %in% c("timestamp","anglex","angley","anglez") == FALSE)[1]
+  accel = as.numeric(as.matrix(M$metashort[,IndOtMetric]))
+
   accel2 = cumsum(c(0, accel))
   select = seq(1,length(accel2), by = ws2 / ws3)
-  var1 = diff(accel2[round(select)]) / abs(diff(round(select[1:(length(select))])))
-  if (length(timeline) > length(var1)) {
-    var1 = c(var1, rep(0, length(var1) - length(timeline)))
-  } else if (length(timeline) < length(var1)) {
-    var1 = var1[1:length(timeline)]
+  Acceleration = diff(accel2[round(select)]) / abs(diff(round(select[1:(length(select))])))
+  if (length(timeline) > length(Acceleration)) {
+    Acceleration = c(Acceleration, rep(0, length(Acceleration) - length(timeline)))
+  } else if (length(timeline) < length(Acceleration)) {
+    Acceleration = Acceleration[1:length(timeline)]
   }
   
   MEND = length(timeline)
-  ticks = seq(n_ws2_perday, nrow(M$metalong), by = n_ws2_perday)
+  ticks = seq(0, nrow(M$metalong) + n_ws2_perday, by = n_ws2_perday)
   # creating plot functions to avoid duplicated code
-  plotacc = function(timeline, var1, durplot, ticks) {
-    plot(timeline, var1, type = "l", xlab = "24 Hour block relative to start recording", ylab = "Acceleration (g)",
-         bty = "l", lwd = 0.1, xlim = c(0, durplot), ylim = c(0, 0.6), axes = FALSE)
+  plot_acc = function(timeline, Acceleration, durplot, ticks) {
+    plot(timeline, Acceleration, type = "l", xlab = "Day (24 hour blocks relative to start of recording)",
+         ylab = expression(paste("Acceleration (", italic("g"), ")")),
+         bty = "l", lwd = 0.1, xlim = c(0, durplot), ylim = c(0, 0.6), axes = FALSE, cex.lab = 0.8)
     axis(side = 2, at = c(0, 0.2, 0.4, 0.6))
-    axis(side = 1, at = ticks, labels = 1:length(ticks))
+    axis(side = 1, at = ticks, labels = 0:(length(ticks) - 1))
   }
   
-  plotnw = function(timeline, M, durplot, ticks) {
+  plot_nonwear = function(timeline, M, durplot, ticks) {
     plot(timeline, M$metalong$nonwearscore, type = "s",
          xlab = "", ylab = "Non-wear score", axes = F,
-         lwd = 0.1, xlim = c(0,durplot), ylim = c(0, 3))
+         lwd = 0.1, xlim = c(0,durplot), ylim = c(0, 3), cex.lab = 0.8)
     axis(side = 2,at = c(0, 1, 2, 3))
-    axis(side = 1,at = ticks, labels = 1:length(ticks))
+    # axis(side = 1,at = ticks, labels = 1:length(ticks))
   }
   # plot data
   if (mon == 2 | (mon == 4 & dformat == 4)) {
     # Recordings with temperature
     par(fig = c(0,1,0,0.65), new = T)
-    plotacc(timeline, var1, durplot, ticks)
+    plot_acc(timeline, Acceleration, durplot, ticks)
     
     par(fig = c(0, 1, 0.45, 0.80), new = T)
-    plotnw(timeline, M, durplot, ticks)
+    plot_nonwear(timeline, M, durplot, ticks)
     
-    par(fig = c(0, 1, 0.60, 1), new = T)
+    par(fig = c(0, 1, 0.60, 0.95), new = T)
     plot(timeline, M$metalong$temperaturemean[1:MEND], type = "l",
          xlab = "", ylab = "Temp. (C)", axes = F, lwd = 0.1,
-         xlim = c(0, durplot), ylim = c(20,35))
+         xlim = c(0, durplot), ylim = c(20,35), cex.lab = 0.8)
     abline(h = 20, col = "black", lwd = 1, lty = 2)
     abline(h = 35, col = "black", lwd = 1, lty = 2)
     axis(side = 2,at = c(20,35))
-    axis(side = 1,at = ticks, labels = 1:length(ticks))
+    # axis(side = 1,at = ticks, labels = 1:length(ticks))
   } else {
-    # Rcordings without temperature
+    # Recordings without temperature
     par(fig = c(0, 1, 0, 0.80), new = T)
-    plotacc(timeline, var1, durplot, ticks)
+    plot_acc(timeline, Acceleration, durplot, ticks)
 
-    par(fig = c(0, 1, 0.60, 1), new = T)
-    plotnw(timeline, M, durplot, ticks)
+    par(fig = c(0, 1, 0.60, 0.95), new = T)
+    plot_nonwear(timeline, M, durplot, ticks)
   }
 }
