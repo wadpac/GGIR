ffb2eca3ea335278430b5ec32d26f350962b6151
Jairo H Migueles <jairo.hidalgo.migueles@gmail.com>
2022-11-03 17:24:05 +0100

    wording fixes in g.wavread (deprecated)

--- R/g.wavread.R
+++ R/g.wavread.R
@@ -1,3 +1,3 @@
 g.wavread = function(...) {
-  stop("Function g.wavread has been deprecated and has been moved to R package GGIRread and renamed readAxivity")
+  stop("Function g.wavread has been deprecated and has been moved to R package GGIRread and renamed readWav")
 }
