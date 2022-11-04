6221a44989aeae04df8eafac6b5f9c919e04ab36
Jairo H Migueles <jairo.hidalgo.migueles@gmail.com>
2022-11-04 19:20:54 +0100

    g.getbout revised and tests for this function working. To do: test chainof5parts failing

--- R/g.getbout.R
+++ R/g.getbout.R
@@ -7,10 +7,10 @@ g.getbout = function(x, boutduration, boutcriter = 0.8, closedbout = FALSE, bout
     jmvpa = 1 # index for going stepwise through vector p
     Lx = length(x) # Lx is length of vector x
     while (jmvpa <= length(p)) { # go through all epochs that are possibly part of a bout
-      endi = p[jmvpa]+boutduration
+      endi = p[jmvpa] + boutduration
       if (endi <= Lx) { #does bout fall without measurement?
         if (sum(x[p[jmvpa]:endi]) > (boutduration*boutcriter)) {
-          while(sum(x[p[jmvpa]:endi]) > ((endi-p[jmvpa])*boutcriter) & endi <= Lx) {
+          while (sum(x[p[jmvpa]:endi]) > ((endi - p[jmvpa])*boutcriter) & endi <= Lx) {
             endi = endi + 1
           }
           select = p[jmvpa:max(which(p < endi))]
@@ -24,7 +24,7 @@ g.getbout = function(x, boutduration, boutcriter = 0.8, closedbout = FALSE, bout
       } else { #bout does not fall within measurement
         jump = 1
         if (length(p) > 1 & jmvpa > 2) {
-          x[p[jmvpa]] = x[p[jmvpa-1]]
+          x[p[jmvpa]] = x[p[jmvpa - 1]]
         }        
       }
       jmvpa = jmvpa + jump
@@ -38,8 +38,8 @@ g.getbout = function(x, boutduration, boutcriter = 0.8, closedbout = FALSE, bout
   } else if (bout.metric == 2) { # MVPA based on percentage relative to start of bout
     xt = x
     jmvpa = 1
-    while(jmvpa <= length(p)) {
-      endi = p[jmvpa]+boutduration
+    while (jmvpa <= length(p)) {
+      endi = p[jmvpa] + boutduration
       if (endi <= length(x)) { #does bout fall without measurement?
         lengthbout = sum(x[p[jmvpa]:endi])
         if (lengthbout > (boutduration*boutcriter)) {
@@ -49,7 +49,7 @@ g.getbout = function(x, boutduration, boutcriter = 0.8, closedbout = FALSE, bout
         }    	
       } else { #bout does not fall within measurement
         if (length(p) > 1 & jmvpa > 2) {
-          x[p[jmvpa]] = x[p[jmvpa-1]]
+          x[p[jmvpa]] = x[p[jmvpa - 1]]
         }				
       }
       jmvpa = jmvpa + 1
@@ -60,15 +60,15 @@ g.getbout = function(x, boutduration, boutcriter = 0.8, closedbout = FALSE, bout
     x[is.na(x)] = 0 # ignore NA values in the unlikely event that there are any
     xt = x
     #look for breaks larger than 1 minute
-    lookforbreaks = zoo::rollmean(x=x,k=(60/ws3),align="center",fill=rep(0,3)) #
+    lookforbreaks = zoo::rollmean(x = x, k = (60/ws3), align = "center", fill = rep(0,3)) #
     #insert negative numbers to prevent these minutes to be counted in bouts
     xt[lookforbreaks == 0] = -(60/ws3) * boutduration 
     #in this way there will not be bouts breaks lasting longer than 1 minute
-    RM = zoo::rollmean(x=xt,k=boutduration,align="center",fill=rep(0,3)) #,
+    RM = zoo::rollmean(x = xt, k = boutduration, align = "center", fill = rep(0,3)) #,
     p = which(RM > boutcriter)
     starti = round(boutduration/2)
     for (gi in 1:boutduration) {
-      inde = p-starti+(gi-1)
+      inde = p - starti + (gi - 1)
       xt[inde[inde > 0 & inde < length(xt)]] = 2
     }
     x[xt != 2] = 0
@@ -133,69 +133,100 @@ g.getbout = function(x, boutduration, boutcriter = 0.8, closedbout = FALSE, bout
       #look for breaks larger than 1 minute
       # Note: we do + 1 to make sure we look for breaks larger than but not equal to a minute,
       # this is critical when working with 1 minute epoch data
-      lookforbreaks = zoo::rollmean(x=x, k=(60/ws3)+1, align="center", fill=rep(0,3))
+      # we need to append 0's at the beginning and end so that first and last epochs in timeseries do not get ignored
+      zeroes = rep(0, ceiling(60/ws3/2))
+      xtmp = c(zeroes, x, zeroes)
+      lookforbreaks = zoo::rollmean(x = xtmp, k = (60/ws3) + 1, align = "center", fill = rep(0,3))
+      # remove appended zeroes
+      keep = (length(zeroes) + 1):(length(lookforbreaks) - length(zeroes))
       #insert negative numbers to prevent these minutes to be counted in bouts
       #in this way there will not be bouts breaks lasting longer than 1 minute
-      xt[lookforbreaks == 0] = -boutduration 
-      RM = zoo::rollmean(x=xt, k=boutduration, align="center", fill=rep(0,3))
-      p = which(RM >=boutcriter)
+      xtmp[lookforbreaks == 0] = -boutduration
+      xt = xtmp[keep]
+      # append 0's to define boutcriter
+      append = rep(-boutduration, ceiling(boutduration/2))
+      xtmp = c(append, xt, append)
+      RM = zoo::rollmean(x = xtmp, k = boutduration, align = "center", fill = rep(0,3))
+      keep = (length(append) + 1):(length(RM) - length(append))
+      RM = RM[keep]
+      p = which(RM >= boutcriter)
       half1 = floor(boutduration/2)
       half2 = boutduration - half1
       # # only consider windows that at least start and end with value that meets criterium
-      p = c(0, p, 0)
-      if (ws3 > 60) {
-        epochs2check = 1
+      # p = c(0, p, 0)
+      # if (ws3 > 60) {
+      #   epochs2check = 1
+      # } else {
+      #   epochs2check = (60/ws3)
+      # }
+      # p = p[which(p != 0)]
+      # now mark all epochs that are covered by the detected bout/s
+      detected_bouts = split(p, cumsum(c(1, diff(p) != 1)))
+      if (length(detected_bouts) == 1 & length(detected_bouts[[1]]) == 0) {
+        # if no bouts are detected
+        x[which(xt != 2)] = 0
+        x[which(xt == 2)] = 1
+        boutcount = x # distinction not made anymore, but object kept to preserve output structure
       } else {
-        epochs2check = (60/ws3)
-      }
-      for (ii in 1:epochs2check) { # only check the first and last minute of each bout
-        # p are all epochs at the centre of the windows that meet the bout criteria
-        # we want to check the start and end of sequence meets the 
-        # threshold criteria
-        edges = which(diff(p) != 1) 
-        seq_start = p[edges + 1] 
-        zeros = which(seq_start == 0)
-        if (length(zeros) > 0) seq_start = seq_start[-zeros] # remove the appended zero
-        seq_end = p[edges] # bout centre starts
-        zeros = which(seq_end == 0)
-        if (length(zeros) > 0) seq_end = seq_end[-zeros]
-        length_xt = length(xt)
-        # ignore epochs at beginning and end of time time series
-        seq_start = seq_start[which(seq_start > half1 & seq_start < length_xt - half2)]
-        seq_end = seq_end[which(seq_end > half1 & seq_end < length_xt - half2)]
-        # print(seq_start)
-        # print(seq_end)
-        # print(xt[((seq_start - half1)+1):((seq_start + half2)-1)])
-        # check half a bout left of sequence centre:
-        if (length(seq_start) > 0) {
-          for (bi in seq_start) {
-            if (length_xt >= (bi - half1)) {
-              if (xt[(bi - half1) + 1] != 1) { # if it does not meet criteria then remove this p value
-                p = p[-which(p == bi)]
-              }
+        # if bouts are detected
+        for (bout_i in 1:length(detected_bouts)) {
+          bout = detected_bouts[[bout_i]]
+          # find start of bout
+          start_found = FALSE
+          adjust = 0
+          while (isFALSE(start_found)) {
+            start = min(bout) - half1 + adjust
+            if (start < 1) {
+              adjust = adjust + 1
+              next
             }
-          }
-        }
-        # check half a bout right of sequence centre:
-        if (length(seq_end) > 0) {
-          for (bi in seq_end) {
-            if (length_xt >= (bi - half2)) {
-              if (xt[(bi + half2)-1] != 1) {
-                p = p[-which(p == bi)]
-              }
+            if (xt[start] != 1) {
+              # a bout cannot start without meetin threshold crit
+              adjust = adjust + 1
+              next
             }
+            start_period = start:max(bout)
+            look4start = split(xt[start_period], cumsum(c(1, diff(xt[start_period]) != 0)))
+            max_gap = 60/ws3
+            zeros = c()
+            for (i in 1:length(look4start)) {
+              zeros = c(zeros, sum(look4start[[i]] == 0))
+            }
+            if (all(zeros <= max_gap)) start_found = TRUE
+            adjust = adjust + 1
           }
+          # find end of bout
+          end_found = FALSE
+          adjust = 0
+          while (isFALSE(end_found)) {
+            end = max(bout) + half1 - adjust
+            if (end > length(xt)) {
+              adjust = adjust + 1
+              next
+            }
+            if (xt[end] != 1) {
+              # a bout cannot start without meetin threshold crit
+              adjust = adjust + 1
+              next
+            }
+            end_period = min(bout):end
+            look4end = split(xt[end_period], cumsum(c(1, diff(xt[end_period]) != 0)))
+            max_gap = 60/ws3
+            zeros = c()
+            for (i in 1:length(look4start)) {
+              zeros = c(zeros, sum(look4start[[i]] == 0))
+            }
+            if (all(zeros <= max_gap)) end_found = TRUE
+            adjust = adjust + 1
+          }
+          # while (x[start] != 1) start = start + 1
+          # while (x[end] != 1) end = end - 1
+          xt[start:end] = 2
         }
+        x[which(xt != 2)] = 0
+        x[which(xt == 2)] = 1
+        boutcount = x # distinction not made anymore, but object kept to preserve output structure
       }
-      p = p[which(p != 0)]
-      # now mark all epochs that are covered by the remaining windows
-      for (gi in 1:(boutduration - 1)) { # if 1:boutduration, then it goes 1 epoch beyond the period of interest marked with refe
-        inde = p-half1+gi
-        xt[inde[which(inde > 0 & inde < length(xt))]] = 2
-      }
-      x[which(xt != 2)] = 0
-      x[which(xt == 2)] = 1
-      boutcount = x # distinction not made anymore, but object kept to preserve output structure
   }  
-  invisible(list(x=x,boutcount=boutcount))
+  invisible(list(x = x, boutcount = boutcount))
 }
--- tests/testthat/test_ggetbout.R
+++ tests/testthat/test_ggetbout.R
@@ -9,7 +9,7 @@ test_that("g.getbout produces expected output", {
             0, rep(1,18), 0, rep(1,6), 0, rep(1,8), 0, 1, 0, rep(1,3),
             0, rep(1,14), 0, rep(1,7), 0, rep(1,8), 0, rep(1,16), 0,
             rep(1,4), rep(0,11), 1, 1, 0, rep(1,8), 0, rep(1,8), 0, rep(1,27),
-             0, rep(1,12), 0, 1, 1, 0, rep(1,7), 0, 0, 1, 1,
+            0, rep(1,12), 0, 1, 1, 0, rep(1,7), 0, 0, 1, 1,
             0, rep(1,5), 0, 1, 1, 0, rep(1,6), 0, rep(1,28), 0, 1, rep(0,693))
   bm1Eboutcount = c(rep(0,42), rep(1,131), rep(0,11), rep(1,123), rep(0,693))
   expect_that(sum(bm1$x),equals(228))
@@ -21,7 +21,7 @@ test_that("g.getbout produces expected output", {
   set.seed(300)
   bm2 = g.getbout(x=round(runif(1000,0.4,1)),boutduration = 120,boutcriter=0.9,
                   closedbout=FALSE,bout.metric=2,ws3=5)
-
+  
   bm2Ex = c(rep(0,42), rep(1,286), rep(0,672))
   bm2Eboutcount = c(rep(0,42), rep(1,286), rep(0,672))
   
@@ -60,32 +60,87 @@ test_that("g.getbout produces expected output", {
   expect_that(sum(bm5$x),equals(285))
   expect_that(sum(bm5$boutcount),equals(285))
   
-  # Metric 6
-  set.seed(300)
-  xtest = c(rep(0,15), rep(1,6), 0, rep(1,10),rep(0,20))
-  bm6 = g.getbout(x=xtest,boutduration = 10, boutcriter=1,
-                  closedbout=FALSE,bout.metric=6,ws3=60)
-  bm6Ex = c(rep(0,27), rep(1,10), rep(0,20))
-  bm6Eboutcount = c(rep(0,27), rep(1,10), rep(0,20))
-  expect_that(sum(bm6$x),equals(9))
-  # Metric 6 - ability to detect even length bout
-  set.seed(300)
-  xtest = x = c(rep(1, 4), rep(0, 3), rep(1, 6),rep(0, 3), rep(1, 10), 0 , rep(1, 9))
-  bm6b = g.getbout(x=xtest,boutduration = 10, boutcriter=1,
-                  closedbout=FALSE,bout.metric=6,ws3=60)
-  expect_that(sum(bm6b$x),equals(9))
-  expect_that(sum(bm6b$boutcount),equals(9))
+  # -------------
+  # Metric 6 - function to create fake bouts of a given duration and boutcrit
+  # in this way, we can predict the time in bouts that should be detected
+  # -------------
+  dummybout = function(totaldur_min = 60,
+                       ws3 = 5,
+                       boutdur = 10,
+                       boutdur_detect = 10,
+                       boutcriter = 0.8,
+                       nBouts = 1) {
+    x = rep(0, totaldur_min*60/ws3) # initialize vector fo 1 hour
+    boutdur = boutdur * 60/ws3
+    boutdur_detect = boutdur_detect * 60/ws3
+    gaps = boutdur_detect * (1-boutcriter)
+    bout = rep(1, boutdur)
+    # random position for gaps without gaps > 1min
+    gap1min = 60/ws3 + 1
+    longest_gap = gap1min + 1
+    while (longest_gap >= gap1min) {
+      zeroes = sort(sample(x = 2:(length(bout) - 1), size = gaps))
+      temp <- cumsum(c(1, diff(zeroes) - 1))
+      temp2 <- rle(temp)
+      longest_gap_tmp = zeroes[which(temp == with(temp2, values[which.max(lengths)]))]
+      longest_gap = length(longest_gap_tmp)
+      # if longest_gap <= 1min, then the random zeroes generated are fine and this breaks the loop
+    }
+    bout[zeroes] = 0
+    # random position in x to place the bouts
+    sep_between_bouts = boutdur + gap1min #bouts need to be separated at least for more than 1 min
+    sep = sep_between_bouts - 1
+    while (sep < sep_between_bouts) {
+      where = sort(sample(x = 1:(length(x) - boutdur), size = nBouts))
+      if (length(where) < 2) sep = sep_between_bouts + 1 # if only one bout, any position is good
+      if (length(where) >= 2) {
+        sep_bouts = diff(where)
+        sep = min(sep_bouts)
+      }
+    }
+    # impute bouts in x
+    for (start_i in where) x[start_i:(start_i + boutdur - 1)] = bout
+    
+    return(x)
+  }
   
-  # Metric 6 - ability to detect odd length bouts
-  set.seed(300)
-  xtest = x = c(rep(0, 20), rep(1, 9), rep(0, 20))
-  bm6c = g.getbout(x=xtest,boutduration =9, boutcriter=1,
-                   closedbout=FALSE,bout.metric=6,ws3=60)
-  expect_that(sum(bm6c$x),equals(8))
-  # Metric 6 - ability to detect bouts with grace periods
-  xtest = c(rep(0,15), rep(1,6), 0, rep(1,10),rep(0,20))
-  bm6 = g.getbout(x=xtest,boutduration = 10, boutcriter=0.9,
-                  closedbout=FALSE,bout.metric=6,ws3=60)
-  expect_that(sum(bm6$x),equals(16))
-  
+  # -------------
+  # Metric 6 - random tests with variable configurations
+  # -------------
+  for (i in 1:500) {
+    # epoch lenght will be of 5, 15, or 60 seconds
+    ws3 = c(5, 15, 60)[sample(x = 1:3, size = 1)]
+    
+    # duration of the bout to be created with dummybout()
+    # from 5 to 30 min in 5-min increments
+    boutdur = c(5, 10, 15, 20, 25, 30)[sample(x = 1:6, size = 1)]
+    
+    # boutdur in minutes to detect bouts with g.getbout 
+    # (this should always be lower than boutdur)
+    boutdur_detect = 60
+    while (boutdur_detect > boutdur) {
+      boutdur_detect = c(5, 10, 15, 25, 30)[sample(x = 1:5, size = 1)]
+    }
+    
+    # boutcriter, one of 0.8, 0.9, 1
+    boutcriter = c(0.8, 0.9, 1)[sample(x = 1:3, size = 1)]
+    
+    # number of bouts to be created
+    nBouts = c(1, 2, 3)[sample(x = 1:3, size = 1)]
+    
+    # dummy bout to test the function, we will run a vector of 24 hours with 
+    # randomly allocated nBouts
+    x = dummybout(totaldur_min = 1440, ws3 = ws3, boutdur = boutdur, boutdur_detect = boutdur_detect, 
+                  boutcriter = boutcriter, nBouts = nBouts)
+    
+    # bout detection with g.getbout
+    out = g.getbout(x = x, boutduration = boutdur_detect*60/ws3, boutcriter = boutcriter, 
+                    closedbout = FALSE, bout.metric = 6, ws3 = ws3)
+    
+    # expected time in bout to be detected by g.getbout
+    expected_bout = (boutdur*60/ws3)*nBouts
+    
+    # test
+    expect_equal(sum(out$x == 1), expected_bout)
+  }
 })
