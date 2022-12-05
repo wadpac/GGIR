797760f272ffd96215214c05fa840031e5365f07
Vincent van Hees <v.vanhees@accelting.com>
2022-12-05 18:38:25 +0100

    turning on last part of unit test

--- tests/testthat/test_recordingEndSleepHour.R
+++ tests/testthat/test_recordingEndSleepHour.R
@@ -40,27 +40,27 @@ test_that("recordingEndSleepHour works as expected", {
   )
   
   
-  # # No warning, this should work
-  # GGIR(datadir = fn, outputdir = getwd(),
-  #        studyname = "test", overwrite = TRUE, desiredtz = tz,
-  #        recordingEndSleepHour = 20,
-  #        minimum_MM_length.part5 = 15)
-  # rn = dir("output_test/meta/basic/",full.names = TRUE)
-  # load(rn[1])
-  # expect_true(nrow(M$metashort) > 43020 ) # metashort is expanded
-  # 
-  # # expanded time is not reports
-  # p2 = read.csv("output_test/results/part2_daysummary.csv")
-  # expect_equal(p2$N.hours[nrow(p2)], 20.75)  # N hours in last day does not include the expanded time
-  # 
-  # p4 = read.csv("output_test/results/part4_nightsummary_sleep_cleaned.csv") 
-  # p4full = read.csv("output_test/results/QC/part4_nightsummary_sleep_full.csv") 
-  # expect_equal(nrow(p4), 2) # Night 3 is not in the part 4 reports
-  # expect_equal(nrow(p4full), 2) # Night 3 is not in the part 4 reports
-  # 
-  # p5 = read.csv("output_test/results/part5_daysummary_MM_L40M100V400_T5A5.csv") 
-  # expect_equal(nrow(p5), 3) # expanded day appears in MM report
-  # expect_true(p5$dur_day_spt_min[nrow(p5)] < 23*60) # but expanded time is not accounted for in estimates
+  # No warning, this should work
+  GGIR(datadir = fn, outputdir = getwd(),
+         studyname = "test", overwrite = TRUE, desiredtz = tz,
+         recordingEndSleepHour = 20,
+         minimum_MM_length.part5 = 6)
+  rn = dir("output_test/meta/basic/",full.names = TRUE)
+  load(rn[1])
+  expect_true(nrow(M$metashort) > 43020 ) # metashort is expanded
+
+  # expanded time is not reports
+  p2 = read.csv("output_test/results/part2_daysummary.csv")
+  expect_equal(p2$N.hours[nrow(p2)], 20.75)  # N hours in last day does not include the expanded time
+
+  p4 = read.csv("output_test/results/part4_nightsummary_sleep_cleaned.csv")
+  p4full = read.csv("output_test/results/QC/part4_nightsummary_sleep_full.csv")
+  expect_equal(nrow(p4), 2) # Night 3 is not in the part 4 reports
+  expect_equal(nrow(p4full), 2) # Night 3 is not in the part 4 reports
+
+  p5 = read.csv("output_test/results/part5_daysummary_MM_L40M100V400_T5A5.csv")
+  expect_equal(nrow(p5), 3) # expanded day appears in MM report
+  expect_true(p5$dur_day_spt_min[nrow(p5)] < 23*60) # but expanded time is not accounted for in estimates
   # 
   
   if (file.exists("output_test"))  unlink("output_test", recursive = TRUE)
