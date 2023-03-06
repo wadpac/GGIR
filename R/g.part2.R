98b3f6af5b15bdc99b3e84cd3846daf93e0ae90e
Vincent van Hees <v.vanhees@accelting.com>
2023-03-06 10:46:30 +0100

    adding function names to functions2passson which is only used GGIR by developers

--- R/g.part2.R
+++ R/g.part2.R
@@ -237,7 +237,8 @@ g.part2 = function(datadir = c(), metadatadir = c(), f0 = c(), f1 = c(),
       functions2passon = c("g.analyse", "g.impute", "g.weardec", "g.detecmidnight",
                            "g.extractheadervars", "g.analyse.avday", "g.getM5L5", "g.IVIS",
                            "g.analyse.perday", "g.getbout", "g.analyse.perfile", "g.intensitygradient",
-                           "iso8601chartime2POSIX", "extract_params", "load_params", "check_params")
+                           "iso8601chartime2POSIX", "extract_params", "load_params", "check_params",
+                           "correctOlderMilestoneData", "cosinorAnalyses")
       errhand = 'stop'
     }
     i = 0 # declare i because foreach uses it, without declaring it
