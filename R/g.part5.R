abaa62d6ed7d1bfb7364ee10811ca2c585f33b1d
Vincent van Hees <v.vanhees@accelting.com>
2023-07-13 09:05:32 +0200

    changing fix because what I did for if-statement makes no sense

--- R/g.part5.R
+++ R/g.part5.R
@@ -279,7 +279,7 @@ g.part5 = function(datadir = c(), metadatadir = c(), f0=c(), f1=c(),
           time_POSIX = iso8601chartime2POSIX(ts$time,tz = params_general[["desiredtz"]])
           tempp = as.POSIXlt(time_POSIX) #unclass(time_POSIX)
           if (is.na(tempp$sec[1]) == TRUE) {
-            tempp = unclass(as.POSIXct(ts$time, tz = params_general[["desiredtz"]]))
+            tempp = as.POSIXlt(as.POSIXct(ts$time, tz = params_general[["desiredtz"]]))
           }
           sec = tempp$sec
           min = tempp$min
@@ -332,7 +332,7 @@ g.part5 = function(datadir = c(), metadatadir = c(), f0=c(), f1=c(),
               time_POSIX = ts$time
               tempp = as.POSIXlt(time_POSIX) #unclass(time_POSIX)
               if (is.na(tempp$sec[1]) == TRUE) {
-                tempp = unclass(as.POSIXct(ts$time, tz = params_general[["desiredtz"]]))
+                tempp = as.POSIXlt(as.POSIXct(ts$time, tz = params_general[["desiredtz"]]))
               }
               sec = tempp$sec
               min = tempp$min
