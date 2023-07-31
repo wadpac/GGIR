f8e7f5046212e13db0ead9aa8276013aa1b042ef
Jairo H Migueles <jairo@jhmigueles.com>
2023-07-31 14:17:54 +0200

    revise tests according to new handling of nnights

--- tests/testthat/test_gloadlog.R
+++ tests/testthat/test_gloadlog.R
@@ -17,13 +17,15 @@ test_that("gloadlog is able to load different log formats", {
     cat(paste0("\nLocal timezone: ", Sys.timezone()))
     logs1 = g.loadlog(loglocation = fn, coln1 = 2, colid = 1, #nnights = 7,
                       meta.sleep.folder = tempdir , desiredtz = "")
-    expect_equal(nrow(logs1$sleeplog), 7)
+    # after deprecating nnights, the whole sleeplog is read (it contains 8 nights)
+    # ammend tests as needed
+    expect_equal(nrow(logs1$sleeplog), 8) 
     expect_equal(ncol(logs1$sleeplog), 5)
-    expect_equal(logs1$sleeplog$night, as.character(1:7))
-    expect_equal(logs1$sleeplog$sleepwake, c("7:0:2", "7:0:3", "7:0:4", "7:0:5", "7:0:6", "7:0:7", "7:0:8"))
-    expect_equal(nrow(logs1$nonwearlog), 7)
+    expect_equal(logs1$sleeplog$night, as.character(1:8))
+    expect_equal(logs1$sleeplog$sleepwake, c("7:0:2", "7:0:3", "7:0:4", "7:0:5", "7:0:6", "7:0:7", "7:0:8", "7:0:9"))
+    expect_equal(nrow(logs1$nonwearlog), 9)
     expect_equal(ncol(logs1$nonwearlog), 4)
-    expect_equal(nrow(logs1$naplog), 7)
+    expect_equal(nrow(logs1$naplog), 9)
     expect_equal(ncol(logs1$naplog), 6)
     
     # start of accelerometer recording 6 days earlier
@@ -31,13 +33,13 @@ test_that("gloadlog is able to load different log formats", {
     save(ID, rec_starttime, file = "mytestdir/dummyms3.RData")
     logs2 = g.loadlog(loglocation = fn, coln1 = 2, colid = 1, #nnights = 7,
                      meta.sleep.folder = tempdir , desiredtz = "")
-    expect_equal(nrow(logs2$sleeplog), 7)
+    expect_equal(nrow(logs2$sleeplog), 8)
     expect_equal(ncol(logs2$sleeplog), 5)
-    expect_equal(logs2$sleeplog$night, as.character(6:12)) # these are nights in the acc recording
-    expect_equal(logs2$sleeplog$sleepwake, c("7:0:2", "7:0:3", "7:0:4", "7:0:5", "7:0:6", "7:0:7", "7:0:8"))
-    expect_equal(nrow(logs2$nonwearlog), 7)
+    expect_equal(logs2$sleeplog$night, as.character(6:13)) # these are nights in the acc recording
+    expect_equal(logs2$sleeplog$sleepwake, c("7:0:2", "7:0:3", "7:0:4", "7:0:5", "7:0:6", "7:0:7", "7:0:8", "7:0:9"))
+    expect_equal(nrow(logs2$nonwearlog), 9)
     expect_equal(ncol(logs2$nonwearlog), 4)
-    expect_equal(nrow(logs2$naplog), 7)
+    expect_equal(nrow(logs2$naplog), 9)
     expect_equal(ncol(logs2$naplog), 6)
     
     # start of accelerometer recording 3 days later
@@ -45,13 +47,13 @@ test_that("gloadlog is able to load different log formats", {
     save(ID, rec_starttime, file = "mytestdir/dummyms3.RData")
     logs3 = g.loadlog(loglocation = fn, coln1 = 2, colid = 1, #nnights = 7,
                       meta.sleep.folder = tempdir , desiredtz = "")
-    expect_equal(nrow(logs3$sleeplog), 3)
+    expect_equal(nrow(logs3$sleeplog), 4)
     expect_equal(ncol(logs3$sleeplog), 5)
-    expect_equal(logs3$sleeplog$night, as.character(1:3)) # 1:3 because these are the first three nights of the acc recording
-    expect_equal(logs3$sleeplog$sleeponset, c("23:0:5", "23:0:6", "23:0:7"))
-    expect_equal(nrow(logs3$nonwearlog), 3)
+    expect_equal(logs3$sleeplog$night, as.character(1:4)) # 1:3 because these are the first three nights of the acc recording
+    expect_equal(logs3$sleeplog$sleeponset, c("23:0:5", "23:0:6", "23:0:7", "23:0:8"))
+    expect_equal(nrow(logs3$nonwearlog), 5)
     expect_equal(ncol(logs3$nonwearlog), 4)
-    expect_equal(nrow(logs3$naplog), 3)
+    expect_equal(nrow(logs3$naplog), 5)
     expect_equal(ncol(logs3$naplog), 6)
   }
 
