07bde44a941fed39d24739e3a79f28b9b5cc2512
Vincent van Hees <v.vanhees@accelting.com>
2023-09-26 14:49:26 +0200

    Update test_part6.R

--- tests/testthat/test_part6.R
+++ tests/testthat/test_part6.R
@@ -15,20 +15,22 @@ test_that("Part 6 with household co-analysis", {
   if (!dir.exists(dn)) dir.create(path = dn, recursive = TRUE)
   dn2 = paste0(metadatadir, "/meta/basic")
   if (!dir.exists(dn2)) dir.create(path = dn2, recursive = TRUE)
-  
   data(data.metalong)
-  save(data.metalong, file = paste0(dn2, "/meta_800-900-001_left wrist.bin.RData"))
-  save(data.metalong, file = paste0(dn2, "/meta_800-900-002_left wrist.bin.RData"))
-  save(data.metalong, file = paste0(dn2, "/meta_800-900-003_left wrist.bin.RData"))
+  M = data.metalong
+  save(M, file = paste0(dn2, "/meta_800-900-001_left wrist.bin.RData"))
+  save(M, file = paste0(dn2, "/meta_800-900-002_left wrist.bin.RData"))
+  save(M, file = paste0(dn2, "/meta_800-900-003_left wrist.bin.RData"))
   
   # Use time shifts to simulate three household members
+  
   data(data.ts)
-  data.ts$timenum = data.ts$timenum - (5 * 60) 
-  save(data.ts, file = paste0(dn, "/800-900-001_left wrist.RData"))
-  data.ts$timenum = data.ts$timenum + (7 * 60)
-  save(data.ts, file = paste0(dn, "/800-900-002_left wrist.RData"))
-  data.ts$timenum = data.ts$timenum + (14 * 60)
-  save(data.ts, file = paste0(dn, "/800-900-003_left wrist.RData"))
+  mdat = data.ts
+  mdat$timenum = mdat$timenum - (5 * 60) 
+  save(mdat, file = paste0(dn, "/800-900-001_left wrist.RData"))
+  mdat$timenum = mdat$timenum + (7 * 60)
+  save(mdat, file = paste0(dn, "/800-900-002_left wrist.RData"))
+  mdat$timenum = mdat$timenum + (14 * 60)
+  save(mdat, file = paste0(dn, "/800-900-003_left wrist.RData"))
   
   # Run household co-analysis  
   # Update parameters to align with datset
@@ -64,7 +66,7 @@ test_that("Part 6 with household co-analysis", {
   expect_equal(sum(PC$wakeup_acc_1_before_2), 171.863)
   expect_equal(PC$wakeup_time1[c(1, 8, 16)], c("01:35:00", "02:58:00", "03:39:00"))
   expect_equal(sum(PC$day_Kappa_active), 5.091)
-
+  
   
   # Run Circadian rhythm analysis  
   params_247[["part6CoAnalysis"]] = FALSE
@@ -74,8 +76,9 @@ test_that("Part 6 with household co-analysis", {
           params_general = params_general,
           params_phyact = params_phyact,
           params_247 = params_247, verbose = FALSE
-          )
+  )
   
   # Remove test files
   if (file.exists(metadatadir))  unlink(metadatadir, recursive = TRUE)
-})
+}
+)
