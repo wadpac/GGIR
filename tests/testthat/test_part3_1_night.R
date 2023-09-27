73324e1399627c4007e63f5c7753eccbced741bd
l-k- <alenka0000@hotmail.com>
2023-09-27 15:48:02 -0400

    simplify test because error reporting in part 3 got simplified
    Error reporting in part 3 is got simplified in this change: https://github.com/wadpac/GGIR/commit/535964ecb8eee0fd630bb45d97c1ab1be3ff95c0
    
    Errors now actually get thrown (and caught by the test) rather than only printed to the console like before.
    So no need to capture output anymore.

--- tests/testthat/test_part3_1_night.R
+++ tests/testthat/test_part3_1_night.R
@@ -9,24 +9,11 @@ test_that("Part 3 identifies 1-night scenario", {
   fn = "123A_testaccfile.csv"
   dn = "output_test"
   if (file.exists(dn)) unlink(dn, recursive = TRUE)
-  # g.part3 modules report errors and warnings only by printing them out to the console,
-  # so these are not true errors and warnings that would get caught by the testing module.
-  # Instead, grab the console output and parse out any errors we should know about.
-  # 
-  # Note: make sure to call GGIR() with do.parallel = FALSE,
-  # otherwise not all console output will be captured.
-  out = capture.output(GGIR(mode = c(1:3), datadir = fn, outputdir = getwd(), studyname = "test", f0 = 1, f1 = 1,
-                                    do.report = c(), visualreport = FALSE, viewingwindow = 1,
-                                    overwrite = TRUE, do.parallel = FALSE, minimumFileSizeMB = 0), 
-                       type = c("output"))
+
+  GGIR(mode = c(1:3), datadir = fn, outputdir = getwd(), studyname = "test", f0 = 1, f1 = 1,
+       do.report = c(), visualreport = FALSE, viewingwindow = 1,
+       overwrite = TRUE, do.parallel = FALSE, minimumFileSizeMB = 0)
   
-  # There shouldn't be any errors or warnings reported, so if there are any, throw an error.
-  err_idx = grepl("Errors and warnings for", out)
-  for (ii in 1:length(err_idx)) {
-    if (err_idx[ii]) {
-      stop(out[ii:length(err_idx)])
-    }
-  }
   out.file.path = paste0(dn, "/meta/ms3.out/", fn, ".RData")
   expect_true(file.exists(out.file.path))
   load(out.file.path)
