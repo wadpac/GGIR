f5fd9c43ba5ccc22907abdbce5be5a90522bc829
Jairo H Migueles <jairo.hidalgo.migueles@gmail.com>
2023-11-27 12:53:43 +0100

    speeding up tests for create_test_acc_csv by reducing the recording lengths and sf

--- R/create_test_acc_csv.R
+++ R/create_test_acc_csv.R
@@ -49,7 +49,7 @@ create_test_acc_csv = function(sf = 3, Nmin = 2000, storagelocation = c(),
   #==================================
   
   if (length(storagelocation) == 0) storagelocation = getwd()
-  if (Nmin < 720) Nmin = 720 # only make this file for tests with at least 12 hours of data
+  # if (Nmin < 720) Nmin = 720 # only make this file for tests with at least 12 hours of data
   if (length(start_time) == 0 || start_time == "") {
     if (starts_at_midnight) {
       start_time = "00:00:00"
@@ -89,106 +89,111 @@ create_test_acc_csv = function(sf = 3, Nmin = 2000, storagelocation = c(),
   testdata[,3] = accz + round(rnorm(n = Nrows, mean = 0.03, sd = 0.02), digits = 4) + #no gravity
     sin(((1:Nrows)/((Nrows)/(Nsamplesinday/0.1)))) * 0.05 +
     sin(((1:Nrows)/((Nrows)/(Nsamplesinday/2)))) * 0.1
-  
   #======================================
-  # generate 2.5-hour sleep blocks: at 12 hours from the start of the recording, and then every 24 hours
-  sleep1 = generate_daily_timestamp_blocks(first_onset = 12 * 3600 * sf,
-                                           block_duration = 2.5 * 3600 * sf, sf = sf,
-                                           max_timestamp = Nrows)
-  
-  # generate 3-hour sleep blocks: at 15 hours from the start of the recording, and then every 24 hours
-  sleep2 = generate_daily_timestamp_blocks(first_onset = 15 * 3600 * sf,
-                                           block_duration = 3 * 3600 * sf,
+  # only insert sleep, rotations,... for tests with at least 12 hours of data
+  if (Nmin >= 720) {
+    #======================================
+    # generate 2.5-hour sleep blocks: at 12 hours from the start of the recording, and then every 24 hours
+    sleep1 = generate_daily_timestamp_blocks(first_onset = 12 * 3600 * sf,
+                                             block_duration = 2.5 * 3600 * sf, sf = sf,
+                                             max_timestamp = Nrows)
+    
+    # generate 3-hour sleep blocks: at 15 hours from the start of the recording, and then every 24 hours
+    sleep2 = generate_daily_timestamp_blocks(first_onset = 15 * 3600 * sf,
+                                             block_duration = 3 * 3600 * sf,
+                                             sf = sf, max_timestamp = Nrows)
+    
+    sleep_periods = sort(unique(c(sleep1,sleep2)))
+    
+    set.seed(300)
+    testdata[sleep_periods, 1] = accx + round(rnorm(n = length(sleep_periods),mean = 0, sd = 0.015), digits = 4)
+    set.seed(400)
+    testdata[sleep_periods, 2] = accy + round(rnorm(n = length(sleep_periods),mean = 0, sd = 0.015), digits = 4)
+    set.seed(500)
+    testdata[sleep_periods, 3] = accz + round(rnorm(n = length(sleep_periods),mean = 0, sd = 0.015), digits = 4)
+    
+    # add some rotations to the acceleormeter, to avoid non-wear detection
+    for (j in 1:(Nrows/(sf*3600))) { # loop to add angle changes to every night
+      t0v = seq(12 + ((j - 1) * 24), 22 + ((j - 1) * 24), by = 0.25) # start times of angle positions
+      phiv = 1:length(t0v)
+      phiv = scale(x = phiv,center = 0,scale = diff(range(phiv))/(2*pi))  # rescale such that angle goes 180 degrees
+      gammav = scale(x = phiv,center = 0,scale = diff(range(phiv))/(2*pi))  # rescale such that angle goes 180 degrees
+      for (i in 1:length(t0v)) {
+        B = gensequence(t0 = t0v[i], t1 = t0v[i] + 0.245) #long activity starts at the 3rd hour of every 24 hours
+        Rm_phi = genrotationmatrix(phiv[i])
+        Rm_gamma = genrotationmatrix(gammav[i])
+        Ry = Rm_phi$Ry; Rx = Rm_gamma$Rx
+        B = B[which(B < nrow(testdata))]
+        if (length(B) > 0) {
+          testdata[B,1] = accx # set constant such that it is detected as a period without movement
+          testdata[B,2] = accy
+          testdata[B,3] = accz
+          testdata[B,] = t(Ry %*% t(testdata[B,]))
+          testdata[B,] = t(Rx %*% t(testdata[B,]))
+        }
+      }
+    }
+    #============================================================
+    # insert 2-minute blocks of activity at the beginning of every 15 minutes
+    act2_15 = generate_repeated_timestamp_blocks(first_onset = 0, block_duration = 2 * 60 * sf,
+                                                 period_length = 15 * 60 * sf,
+                                                 sf = sf,
+                                                 max_timestamp = Nrows)
+    
+    ACC = rep(0,Nrows) #empty ACC pattern
+    set.seed(300)
+    ACC[act2_15] = 0.08 + round(rnorm(n = length(act2_15),
+                                      mean = 0, sd = 0.01) , digits = 4) # add 0.08g + 10 mg noise
+    testdata[,1] = testdata[,1] + ACC
+    #======================================
+    # generate one 2-hour block of non-wear starting at the 5th minute of every 24 hours
+    nw = generate_daily_timestamp_blocks(first_onset = 5 * 60 * sf,
+                                         block_duration = 2 * 3600 * sf,
+                                         sf = sf, max_timestamp = Nrows)
+    
+    testdata[nw,1] = accx # constant values to trigger non-wear detection
+    testdata[nw,2] = accy
+    testdata[nw,3] = accz
+    #======================================
+    # insert 20 and 4 minute blocks of activity
+    
+    # generate one 20-minute block of activity starting at the 3rd hour of every 24 hours
+    act1 = generate_daily_timestamp_blocks(first_onset = 3 * 3600 * sf,
+                                           block_duration = 20 * 60 * sf,
                                            sf = sf, max_timestamp = Nrows)
-  
-  sleep_periods = sort(unique(c(sleep1,sleep2)))
-  
-  set.seed(300)
-  testdata[sleep_periods, 1] = accx + round(rnorm(n = length(sleep_periods),mean = 0, sd = 0.015), digits = 4)
-  set.seed(400)
-  testdata[sleep_periods, 2] = accy + round(rnorm(n = length(sleep_periods),mean = 0, sd = 0.015), digits = 4)
-  set.seed(500)
-  testdata[sleep_periods, 3] = accz + round(rnorm(n = length(sleep_periods),mean = 0, sd = 0.015), digits = 4)
-  
-  # add some rotations to the acceleormeter, to avoid non-wear detection
-  for (j in 1:(Nrows/(sf*3600))) { # loop to add angle changes to every night
-    t0v = seq(12 + ((j - 1) * 24), 22 + ((j - 1) * 24), by = 0.25) # start times of angle positions
+    
+    # generate one 4-minute block of activity starting at the 3.5th hour of every 24 hours
+    act2 = generate_daily_timestamp_blocks(first_onset = 3.5 * 3600 * sf,
+                                           block_duration = 4 * 60 * sf,
+                                           sf = sf, max_timestamp = Nrows)
+    
+    act_periods = sort(unique(c(act1,act2)))
+    
+    ACC = rep(0,Nrows) #empty ACC pattern
+    set.seed(300)
+    ACC[act_periods] = 0.5 + round(rnorm(n = length(act_periods),
+                                         mean = 0, sd = 0.04), digits = 4) # add 0.5g + 20 mg noise
+    testdata[,1] = testdata[,1] + ACC
+    #======================================
+    # insert variations in angle to facilitate autocalibration
+    t0v = seq(4.5, 9, by = 1/120) # start times of angle positions, and 30 seconds each
     phiv = 1:length(t0v)
-    phiv = scale(x = phiv,center = 0,scale = diff(range(phiv))/(2*pi))  # rescale such that angle goes 180 degrees
-    gammav = scale(x = phiv,center = 0,scale = diff(range(phiv))/(2*pi))  # rescale such that angle goes 180 degrees
+    phiv = scale(x = phiv, center = 0, scale = diff(range(phiv)) / (20 * pi))  # rescale such that angle go 4 times 360 degrees
+    gammav = scale(x = phiv, center = 0, scale = diff(range(phiv)) / (2 * pi))  # rescale such that angle goes 180 degrees
     for (i in 1:length(t0v)) {
-      B = gensequence(t0 = t0v[i], t1 = t0v[i] + 0.245) #long activity starts at the 3rd hour of every 24 hours
+      B = gensequence(t0 = t0v[i], t1 = t0v[i] + 0.24) #long activity starts at the 3rd hour of every 24 hours
       Rm_phi = genrotationmatrix(phiv[i])
       Rm_gamma = genrotationmatrix(gammav[i])
       Ry = Rm_phi$Ry; Rx = Rm_gamma$Rx
-      B = B[which(B < nrow(testdata))]
-      if (length(B) > 0) {
-        testdata[B,1] = accx # set constant such that it is detected as a period without movement
-        testdata[B,2] = accy
-        testdata[B,3] = accz
-        testdata[B,] = t(Ry %*% t(testdata[B,]))
-        testdata[B,] = t(Rx %*% t(testdata[B,]))
-      }
+      testdata[B,1] = accx # set constant such that it is detected as a period without movement
+      testdata[B,2] = accy
+      testdata[B,3] = accz
+      testdata[B,] = t(Ry %*% t(testdata[B,]))
+      testdata[B,] = t(Rx %*% t(testdata[B,]))
     }
   }
-  #============================================================
-  # insert 2-minute blocks of activity at the beginning of every 15 minutes
-  act2_15 = generate_repeated_timestamp_blocks(first_onset = 0, block_duration = 2 * 60 * sf,
-                                               period_length = 15 * 60 * sf,
-                                               sf = sf,
-                                               max_timestamp = Nrows)
-  
-  ACC = rep(0,Nrows) #empty ACC pattern
-  set.seed(300)
-  ACC[act2_15] = 0.08 + round(rnorm(n = length(act2_15),
-                                    mean = 0, sd = 0.01) , digits = 4) # add 0.08g + 10 mg noise
-  testdata[,1] = testdata[,1] + ACC
   #======================================
-  # generate one 2-hour block of non-wear starting at the 5th minute of every 24 hours
-  nw = generate_daily_timestamp_blocks(first_onset = 5 * 60 * sf,
-                                       block_duration = 2 * 3600 * sf,
-                                       sf = sf, max_timestamp = Nrows)
-  
-  testdata[nw,1] = accx # constant values to trigger non-wear detection
-  testdata[nw,2] = accy
-  testdata[nw,3] = accz
-  #======================================
-  # insert 20 and 4 minute blocks of activity
-  
-  # generate one 20-minute block of activity starting at the 3rd hour of every 24 hours
-  act1 = generate_daily_timestamp_blocks(first_onset = 3 * 3600 * sf,
-                                         block_duration = 20 * 60 * sf,
-                                         sf = sf, max_timestamp = Nrows)
-  
-  # generate one 4-minute block of activity starting at the 3.5th hour of every 24 hours
-  act2 = generate_daily_timestamp_blocks(first_onset = 3.5 * 3600 * sf,
-                                         block_duration = 4 * 60 * sf,
-                                         sf = sf, max_timestamp = Nrows)
-  
-  act_periods = sort(unique(c(act1,act2)))
-  
-  ACC = rep(0,Nrows) #empty ACC pattern
-  set.seed(300)
-  ACC[act_periods] = 0.5 + round(rnorm(n = length(act_periods),
-                                       mean = 0, sd = 0.04), digits = 4) # add 0.5g + 20 mg noise
-  testdata[,1] = testdata[,1] + ACC
-  #======================================
-  # insert variations in angle to facilitate autocalibration
-  t0v = seq(4.5, 9, by = 1/120) # start times of angle positions, and 30 seconds each
-  phiv = 1:length(t0v)
-  phiv = scale(x = phiv, center = 0, scale = diff(range(phiv)) / (20 * pi))  # rescale such that angle go 4 times 360 degrees
-  gammav = scale(x = phiv, center = 0, scale = diff(range(phiv)) / (2 * pi))  # rescale such that angle goes 180 degrees
-  for (i in 1:length(t0v)) {
-    B = gensequence(t0 = t0v[i], t1 = t0v[i] + 0.24) #long activity starts at the 3rd hour of every 24 hours
-    Rm_phi = genrotationmatrix(phiv[i])
-    Rm_gamma = genrotationmatrix(gammav[i])
-    Ry = Rm_phi$Ry; Rx = Rm_gamma$Rx
-    testdata[B,1] = accx # set constant such that it is detected as a period without movement
-    testdata[B,2] = accy
-    testdata[B,3] = accz
-    testdata[B,] = t(Ry %*% t(testdata[B,]))
-    testdata[B,] = t(Rx %*% t(testdata[B,]))
-  }
+  # file structure and save
   testfile = matrix(" ", Nrows + 11, 3)
   testfile[1:10,1] = header
   testfile[11,1:3] = variablenames
--- tests/testthat/test_create_test_acc_csv.R
+++ tests/testthat/test_create_test_acc_csv.R
@@ -1,23 +1,26 @@
 library(GGIR)
 context("create_test_acc_csv")
 test_that("create_test_acc_csv produces a file", {
-  create_test_acc_csv()
+  t0 = Sys.time()
+  # creates file with correct filename
+  create_test_acc_csv(Nmin = 10)
   fn = "123A_testaccfile.csv"
   expect_true(file.exists(fn))
   if (file.exists(fn)) file.remove(fn)
 
   # generate a test file with a non-wear block straddling the end of the recording 
-  create_test_acc_csv(Nmin = 3000)
+  create_test_acc_csv(Nmin = 1500, sf = 1)
   expect_true(file.exists(fn))
   if (file.exists(fn)) file.remove(fn)
 
   # generate a test file with a sleep block straddling the end of the recording 
-  create_test_acc_csv(Nmin = 2309)
+  create_test_acc_csv(Nmin = 1155, sf = 1)
   expect_true(file.exists(fn))
   if (file.exists(fn)) file.remove(fn)
 
   # generate a test file with an activity block straddling the end of the recording 
-  create_test_acc_csv(Nmin = 2311)
+  create_test_acc_csv(Nmin = 2311, sf = 1)
   expect_true(file.exists(fn))
   if (file.exists(fn)) file.remove(fn)
+  print(Sys.time() - t0)
 })
