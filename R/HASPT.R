d0d5f138a552d365d375860ab47636360ecbd81b
Vincent van Hees <v.vanhees@accelting.com>
2024-01-11 16:40:59 +0100

    revise order to be more intuitive, add documentation, and add to changelog #1011

--- R/HASPT.R
+++ R/HASPT.R
@@ -58,11 +58,11 @@ HASPT = function(angle, perc = 10, spt_threshold = 15,
       # However, we need to take into account that there may be some
       # noise in the data, so threshold needs to be above zero
       x = activity
-      activityThreshold = sd(x) * 0.2
-      # smooth x to 5 minute rolling average to reduce sensitivity to sudden peaks
-      ma <- function(x, n = 300 / ws3){stats::filter(x, rep(1 / n, n), sides = 2)}
-      x = ma(x)
       
+      # smooth x to 5 minute rolling average to reduce sensitivity to sudden peaks
+      ma <- function(x, n = 300 / ws3){stats::filter(x, rep(1 / n, n), sides = 2, circular = TRUE)}
+      x = ma(x)
+      activityThreshold = sd(x, na.rm = TRUE) * 0.2
       if (HASPT.ignore.invalid == TRUE) {
         invalid = adjustlength(x, invalid)
         zeroMovement = which(x <= activityThreshold & invalid == 0)
