596beacdff1421dae18953e4bf2f117db26bda25
l-k- <alenka0000@hotmail.com>
2024-01-18 17:38:03 -0500

    convert numeric timestamps directly to desiredtz
    Unix timestamps are always in UTC, so converting them first to configtz then to deziredtz is the same as converting them to desiredtz right away.

--- R/read.myacc.csv.R
+++ R/read.myacc.csv.R
@@ -250,7 +250,7 @@ read.myacc.csv = function(rmc.file=c(), rmc.nrow=Inf, rmc.skip=c(), rmc.dec=".",
     } else if (rmc.unit.time == "character") {
       P$time = as.POSIXct(P$time,format = rmc.format.time, tz = configtz)
     } else if (rmc.unit.time == "UNIXsec") {
-      P$time = as.POSIXct(P$time, origin = rmc.origin, tz = configtz)
+      P$time = as.POSIXct(P$time, origin = rmc.origin, tz = desiredtz)
     } else if (rmc.unit.time == "ActivPAL") {
       # origin should be specified as: "1899-12-30"
       rmc.origin = "1899-12-30"
@@ -258,15 +258,14 @@ read.myacc.csv = function(rmc.file=c(), rmc.nrow=Inf, rmc.skip=c(), rmc.dec=".",
       tmp2 = P$time - round(P$time)
       timecode = ((tmp2 * 10^10) * 8.64) / 1000000
       numerictime = datecode + timecode
-      P$time = as.POSIXct(numerictime, origin = rmc.origin, tz = configtz)
+      P$time = as.POSIXct(numerictime, origin = rmc.origin, tz = desiredtz)
     }
     if (length(which(is.na(P$time) == FALSE)) == 0) {
       stop("\nExtraction of timestamps unsuccesful, check timestamp format arguments")
     }
   }
-  if (configtz != desiredtz) {
-    P$time = as.POSIXct(as.numeric(P$time),
-                                 tz = desiredtz, origin = "1970-01-01")
+  if (!(rmc.unit.time %in% c("UNIXsec", "ActivPAL")) && (configtz != desiredtz)) {
+    P$time = as.POSIXct(as.numeric(P$time), tz = desiredtz, origin = "1970-01-01")
   }
   
   # If acceleration is stored in mg units then convert to gravitational units
