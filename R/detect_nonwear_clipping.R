0eeaf6dfb478f9cda1ef4444117a0572518743ab
l-k- <alenka0000@hotmail.com>
2024-02-05 20:13:52 -0500

    fix useNA parameter for table()

--- R/detect_nonwear_clipping.R
+++ R/detect_nonwear_clipping.R
@@ -46,9 +46,8 @@ detect_nonwear_clipping = function(data = c(), windowsizes = c(5, 900, 3600), sf
       }
       # ---
       if ("wear" %in% colnames(data)) {
-        wearcol = as.character(data[, "wear"])
-        suppressWarnings(storage.mode(wearcol) <- "logical")
-        wearTable = table(wearcol[(1 + hoc1):hoc2], useNA = FALSE)
+        wearcol = as.logical(data[, "wear"])
+        wearTable = table(wearcol[(1 + hoc1):hoc2], useNA = "no")
         NWav[h] = as.logical(tail(names(sort(wearTable)), 1)) * 3 # times 3 to simulate heuristic approach
       }
       xyzCol = which(colnames(data) %in% c("x", "y", "z"))
