dc3c29dca5e062b5b8a7ddc1c5018db24acc67cd
l-k- <alenka0000@hotmail.com>
2024-02-12 10:26:15 -0500

    use meantemp instead of meantempcal for some types
    for GENEActin bin and for Movisens, use locally calculated mean temperature instead of meantempcal that was calculated in g.calibrate

--- R/g.getmeta.R
+++ R/g.getmeta.R
@@ -330,6 +330,9 @@ g.getmeta = function(datafile, params_metrics = c(), params_rawdata = c(),
           yy = cbind(temperature,
                      temperature,
                      temperature)
+          if ((mon == MONITOR$GENEACTIV && dformat == FORMAT$BIN) || mon == MONITOR$MOVISENS) {
+            meantempcal = mean(temperature, na.rm = TRUE)
+          }
           data[, c("x", "y", "z")] = data[, c("x", "y", "z")] + scale(yy, center = rep(meantempcal,3), scale = 1/tempoffset)
         }
 
