16b97f05ca0740851ffd3583230f161cacd9da42
Vincent van Hees <v.vanhees@accelting.com>
2024-02-21 08:03:42 +0100

    Export namespace and LC_TIME to parallel process
    This is part of addressing #1048 and complements PR #1049 but I am adding this in PR #1028 because of all the changes to part 6 in this PR.

--- R/g.part6.R
+++ R/g.part6.R
@@ -312,6 +312,12 @@ g.part6 = function(datadir = c(), metadatadir = c(), f0 = c(), f1 = c(),
         Ncores2use = min(c(Ncores - 1, params_general[["maxNcores"]], (f1 - f0) + 1))
         if (Ncores2use > 1) {
           cl <- parallel::makeCluster(Ncores2use) # not to overload your computer
+          parallel::clusterExport(cl = cl, 
+                                  varlist = c(unclass(lsf.str(envir = asNamespace("GGIR"), all = T)),
+                                              "MONITOR", "FORMAT"),
+                                  envir = as.environment(asNamespace("GGIR"))
+          )
+          parallel::clusterEvalQ(cl, Sys.setlocale("LC_TIME", "C"))
           doParallel::registerDoParallel(cl)
         } else {
           # Don't process in parallel if only one core
