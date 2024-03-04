7b6562657e0977ae31cabf1cd90d92b3669b877d
Vincent van Hees <v.vanhees@accelting.com>
2024-03-04 15:01:50 +0100

    make parameter delta more explicit in code

--- R/g.fragmentation.R
+++ R/g.fragmentation.R
@@ -42,8 +42,9 @@ g.fragmentation = function(frag.metrics = c("mean", "TP", "Gini", "power",
         Nba = length(ba)
         totDur_a = sum(frag$length[which(frag$value %in% a)]) - ifelse(frag$value[Nsegments] %in% a,1,0)
         totDur_b = sum(frag$length[which(frag$value %in% b)]) - ifelse(frag$value[Nsegments] %in% b,1,0)
-        TPab = (Nab + 1e-6 ) / (totDur_a + 1e-6)
-        TPba = (Nba + 1e-6 ) / (totDur_b + 1e-6)
+        delta = 1e-6
+        TPab = (Nab + delta) / (totDur_a + delta)
+        TPba = (Nba + delta) / (totDur_b + delta)
         # Round to 6 digits because preceding step introduces bias at 7 decimal places
         TPab = round(TPab, digits = 6)
         TPba = round(TPba, digits = 6)
