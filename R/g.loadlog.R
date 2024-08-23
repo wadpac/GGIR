d4ef0e0610b221ae0a5ca01be7a515ed0afabf39
Vincent van Hees <v.vanhees@accelting.com>
2024-08-23 09:52:08 +0200

    handle sleeplog with empty header columns #1138
    and make recognition of sib definitions more robust in part 4

--- R/g.loadlog.R
+++ R/g.loadlog.R
@@ -7,8 +7,10 @@ g.loadlog = function(loglocation = c(), coln1 = c(), colid = c(),
   # Load sleep log data...
   S = data.table::fread(file = loglocation, stringsAsFactors = FALSE, data.table = FALSE,
                         check.names = TRUE, colClasses = "character")
-  S = Filter(function(x)!all(x == ""), S) # remove empty columns
-  nnights = (ncol(S) - coln1 + 1) / 2
+  if (colnames(S)[1] == "V1" && any(S[1, ] == "")) {
+    stop(paste0("Sleeplog column found with empty header, please fix. This can also happen if ",
+                "there are empty columns at the end, delete those columns if applicable."), call. = FALSE)
+  }
   cnt_time_notrecognise = 0
   advanced_sleeplog = length(grep(pattern = "date", x = colnames(S), ignore.case = TRUE)) > 0
   if (advanced_sleeplog ==  TRUE) {
@@ -210,6 +212,8 @@ g.loadlog = function(loglocation = c(), coln1 = c(), colid = c(),
         colid = 1
       }
     }
+  } else {
+    nnights = (ncol(S) - coln1 + 1) / 2
   }
   # test whether number of columns with night information in sleeplog is odd
   # this would provide nnights %% 2 == 0.5
