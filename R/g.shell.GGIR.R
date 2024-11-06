1e11ce4034213126486324fa1f97a5ca1c6c8ae3
Vincent van Hees <v.vanhees@accelting.com>
2024-11-06 13:37:27 +0100

    Update g.shell.GGIR.R

--- R/g.shell.GGIR.R
+++ R/g.shell.GGIR.R
@@ -1,6 +1,6 @@
 g.shell.GGIR = function(...) {
   message(paste0("Please note that instead of g.shell.GGIR() you can just write ",
-                 "GGIR(), because all g.shell.GGIR does is forward its input to ",
+                 "GGIR(), because all g.shell.GGIR() does is forward its input to ",
                  "function GGIR()."), call. = FALSE)
   .args <- as.list(match.call()[-1])
   do.call(GGIR, .args)
