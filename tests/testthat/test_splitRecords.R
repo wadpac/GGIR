305b77f83c6aa0bf50a8aa1deda7e4981957719b
Vincent van Hees <v.vanhees@accelting.com>
2025-03-26 11:25:18 +0100

    improve unit test for splitRecords functionality and also log split names in part 2 csv output #1278

--- tests/testthat/test_splitRecords.R
+++ tests/testthat/test_splitRecords.R
@@ -10,17 +10,31 @@ test_that("Recording can be split", {
   data(data.inspectfile)
   M = data.getmeta
   
+  # duplicate data
+  M$metashort =  rbind(M$metashort, M$metashort)
+  M$metalong =  rbind(M$metalong, M$metalong)
+  starttime = as.POSIXct(M$metashort$timestamp[1], tz = "Europe/London")
+  M$metashort$timestamp = seq(from = starttime, by = 5, length.out = nrow(M$metashort))
+  starttime = as.POSIXct(M$metalong$timestamp[1], tz = "Europe/London")
+  M$metalong$timestamp = seq(from = starttime, by = 900, length.out = nrow(M$metalong))
+  
   # change timestamp format to what it has been since 2017
   M$metashort$timestamp = POSIXtime2iso8601(x = M$metashort$timestamp, tz = "Europe/London")
   M$metalong$timestamp = POSIXtime2iso8601(x = M$metalong$timestamp, tz = "Europe/London")
+  colnames(M$metashort)[which(colnames(M$metashort) == "angle")] = "anglez"
   I = data.inspectfile
   I$filename = "1_1-1-1900.bin"
   C = data.calibrate
   
-  dn = "./testfolder/meta/basic"
+  dn = "./output_test/meta/basic"
   if (!dir.exists(dn)) {
     dir.create(dn, recursive = TRUE)
   }
+  result_folder = "./output_test/results/QC"
+  if (!dir.exists(result_folder)) {
+    dir.create(result_folder, recursive = TRUE)
+  }
+  
   filefoldername = filename_dir = I$filename
   tail_expansion_log = NULL
   save(M, C, I,
@@ -29,7 +43,7 @@ test_that("Recording can be split", {
        tail_expansion_log = tail_expansion_log,
        file = paste0(dn, "/meta_1_1-1-1900.bin.RData"))
   
-  times_to_split = data.frame(ID = "1", time1 = "2013-11-15 00:05", time2 = "2013-11-16")
+  times_to_split = data.frame(ID = "1", time1 = "2013-11-15 00:05", time2 = "2013-11-18")
   csv_file = "time_chunks_per_participant.csv"
   write.csv(x = times_to_split, file = csv_file, row.names = FALSE)
   
@@ -41,31 +55,61 @@ test_that("Recording can be split", {
   params_general[["recording_split_overlap"]] = 0
   params_general[["recording_split_timeformat"]] = "%Y-%m-%d %H:%M"
   
-  splitRecords(metadatadir = "./testfolder",
+  splitRecords(metadatadir = "./output_test",
                params_general = params_general)
   
   # File created?
-  expect_true(dir.exists("./testfolder/meta/basic"))
-  expect_equal(length(dir("./testfolder/meta/basic")), 2)
-  expect_true(file.exists("./testfolder/meta/basic/meta_1_1-1-1900_split1_startrecTOtime1.bin.RData"))
-  expect_true(file.exists("./testfolder/meta/basic/meta_1_1-1-1900_split2_time1TOendrec.bin.RData"))
+  expect_true(dir.exists("./output_test/meta/basic"))
+  expect_equal(length(dir("./output_test/meta/basic")), 2)
+  expect_true(file.exists("./output_test/meta/basic/meta_1_1-1-1900_split1_startrecTOtime1.bin.RData"))
+  expect_true(file.exists("./output_test/meta/basic/meta_1_1-1-1900_split2_time1TOendrec.bin.RData"))
 
   # File content correct?
-  load("./testfolder/meta/basic/meta_1_1-1-1900_split1_startrecTOtime1.bin.RData")
+  load("./output_test/meta/basic/meta_1_1-1-1900_split1_startrecTOtime1.bin.RData")
   expect_equal(nrow(M$metashort), 8100)
   expect_equal(nrow(M$metalong), 45)
   expect_equal(M$metashort$timestamp[1], "2013-11-14T11:45:00+0000")
   expect_equal(tail(M$metashort$timestamp, n = 1), "2013-11-14T22:59:55+0000")
   expect_equal(M$metalong$timestamp[1],  "2013-11-14T11:45:00+0000")
   expect_equal(tail(M$metalong$timestamp, n = 1), "2013-11-14T22:45:00+0000")
-  load("./testfolder/meta/basic/meta_1_1-1-1900_split2_time1TOendrec.bin.RData")
-  expect_equal(nrow(M$metashort), 10619)
-  expect_equal(nrow(M$metalong), 58)
+  load("./output_test/meta/basic/meta_1_1-1-1900_split2_time1TOendrec.bin.RData")
+  expect_equal(nrow(M$metashort), 29339)
+  expect_equal(nrow(M$metalong), 162)
   expect_equal(M$metashort$timestamp[1], "2013-11-14T23:00:00+0000")
-  expect_equal(tail(M$metashort$timestamp, n = 1), "2013-11-15T13:44:50+0000")
+  expect_equal(tail(M$metashort$timestamp, n = 1), "2013-11-16T15:44:50+0000")
   expect_equal(M$metalong$timestamp[1], "2013-11-14T23:00:00+0000")
-  expect_equal(tail(M$metalong$timestamp, n = 1), "2013-11-15T13:15:00+0000")
+  expect_equal(tail(M$metalong$timestamp, n = 1), "2013-11-16T15:15:00+0000")
   
-  if (dir.exists("./testfolder"))  unlink("./testfolder", recursive = TRUE)
+  # Test in context of GGIR as a whole:
+  GGIR(datadir = "D:/test", outputdir = ".",
+       mode = 2:5, do.report = c(2, 4, 5), verbose = FALSE,
+       visualreport = FALSE)
+  
+  # We expect split names to be logged in part 2 csv reports
+  P2 = read.csv("./output_test/results/part2_summary.csv")
+  expect_equal(P2$split1_name, c("startrec", "time1"))
+  expect_equal(P2$split2_name, c("time1", "endrec"))
+  P2day = read.csv("./output_test/results/part2_daysummary.csv")
+  expect_equal(P2day$split1_name, c("startrec", "startrec", "time1", "time1"))
+  expect_equal(P2day$split2_name, c("time1", "time1", "endrec", "endrec"))
+  QC = read.csv("./output_test/results/QC/data_quality_report.csv")
+  expect_equal(QC$split1_name, c("startrec", "time1"))
+  expect_equal(QC$split2_name, c("time1", "endrec"))
+  # We expect split names to be logged in part 4 csv reports
+  P4night = read.csv("./output_test/results/part4_nightsummary_sleep_cleaned.csv")
+  expect_equal(P4night$split1_name, "time1")
+  expect_equal(P4night$split2_name, "endrec")
+  P4 = read.csv("./output_test/results/part4_summary_sleep_cleaned.csv")
+  expect_equal(P4$split1_name, "time1")
+  expect_equal(P4$split2_name, "endrec")
+  # We expect split names to be logged in part 5 csv reports
+  P5day = read.csv("./output_test/results/part5_daysummary_MM_L40M100V400_T5A5.csv")
+  expect_equal(P5day$split1_name, "time1")
+  expect_equal(P5day$split2_name, "endrec")
+  P5 = read.csv("./output_test/results/part5_personsummary_MM_L40M100V400_T5A5.csv")
+  expect_equal(P5$split1_name, "time1")
+  expect_equal(P5$split2_name, "endrec")
+  
+  if (dir.exists("./output_test"))  unlink("./output_test", recursive = TRUE)
   if (file.exists(csv_file)) unlink(csv_file, recursive = TRUE)
 })
