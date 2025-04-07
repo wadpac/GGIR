bf7b9b65165c8197fd4e79c33b7dda2bfa27be77
Vincent van Hees <v.vanhees@accelting.com>
2025-04-07 09:17:57 +0200

    correct unit test following update in previous commit #1278

--- tests/testthat/test_load_check_params.R
+++ tests/testthat/test_load_check_params.R
@@ -18,7 +18,7 @@ test_that("load_params can load parameters", {
   expect_equal(length(params$params_cleaning), 28)
   expect_equal(length(params$params_phyact), 14)
   expect_equal(length(params$params_output), 27)
-  expect_equal(length(params$params_general), 20)
+  expect_equal(length(params$params_general), 21)
 
   params_sleep = params$params_sleep
   params_metrics = params$params_metrics
