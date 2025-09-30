b434952f61eeb367b4dc403183fc4c1c4062d9e2
Vincent van Hees <v.vanhees@accelting.com>
2025-09-30 09:13:20 +0200

    add new external function parameter for events to check_myfun #1396

--- R/check_myfun.R
+++ R/check_myfun.R
@@ -13,7 +13,8 @@ check_myfun = function(myfun, windowsizes) { # Function to check myfun object
                                               "timestamp","reporttype",
                                               "clevels", "ilevels", "qlevels",
                                               "ebout.dur", "ebout.th.cad", "ebout.th.acc",
-                                              "ebout.criter", "ebout.condition", "name") == FALSE)
+                                              "ebout.criter", "ebout.condition", "ebout.cad.perc",
+                                              "name") == FALSE)
   if (length(foreignElements) != 0) {
     status = 1
     stop("Object myfun has unexpected elements.", call. = FALSE)
