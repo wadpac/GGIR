91255a4c1ca83f495daba8da065d376ecda86364
Vincent van Hees <8362333+vincentvanhees@users.noreply.github.com>
2025-12-01 13:52:25 +0100

    revise test for acceleration value to be less system sensitive

--- tests/testthat/test_greadaccfile.R
+++ tests/testthat/test_greadaccfile.R
@@ -454,9 +454,9 @@ test_that("g.readaccfile and g.inspectfile can read movisens, gt3x, cwa, Axivity
                            params_rawdata = params_rawdata, params_general = params_general)
   expect_equal(nrow(mtx_read$P$QClog), 4) # 4 blocks of data in this file
 
-  expect_equal(sum(round(mtx_read$P$data[c("x")], digits = 4)), 8546.728, tolerance = .1, scale = 1)
-  expect_equal(sum(round(mtx_read$P$data[c("y")], digits = 4)), 10913.9, tolerance = .1, scale = 1)
-  expect_equal(sum(round(mtx_read$P$data[c("z")], digits = 4)), -13005.89, tolerance = .1, scale = 1)
+  expect_equal(mean(mtx_read$P$data[, "x"]), 0.3484907, tolerance = .001, scale = 1)
+  expect_equal(mean(mtx_read$P$data[, "y"]), 0.445011, tolerance = .001, scale = 1)
+  expect_equal(mean(mtx_read$P$data[, "z"]), -0.5303114, tolerance = .001, scale = 1)
 
   Mmtx = g.getmeta(mtxfile, desiredtz = desiredtz, windowsize = c(1,300,300),
                    inspectfileobject = Imtx)
