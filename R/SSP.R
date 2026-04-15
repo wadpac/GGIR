efb3b173e142fc81796cf7d0f995dcba1205db15
jhmigueles <jairo@jhmigueles.com>
2026-04-15 18:00:54 +0200

    implementation of vectorized approach for the DFA analysis

--- R/SSP.R
+++ R/SSP.R
@@ -31,15 +31,66 @@
 #- SSP(sunspot.year, scale = 1.2)
 
 SSP = function(data, scale = 2^(1/8), box_size = 4, m = 1){
+  
+  # Helper to replicate the fluctuation analysis using sapply later
+  calc_fluctuation = function(n, y, N_total) {
+    num_boxes = N_total %/% n
+    W = n * num_boxes
+    y_truncated = y[1:W]
+    
+    # Reshape y into a matrix: each column is one box
+    y_matrix = matrix(y_truncated, nrow = n)
+    
+    # Create the x-vector for a single box: 1:n
+    x = 1:n
+    # Slope (beta) for each box: beta = cov(x,y) / var(x)
+    x_detrend = x - mean(x)
+    sum_x2 = sum(x_detrend^2)
+    
+    # betas is a vector of slopes (one for each box)
+    betas = colSums(y_matrix * x_detrend) / sum_x2
+    
+    # intercepts is a vector of intercepts (one for each box)
+    y_means = colMeans(y_matrix)
+    intercepts = y_means - (betas * mean(x))
+    
+    # Fits for every point
+    # fit_matrix[i, j] = intercepts[j] + betas[j] * x[i]
+    betas_mtx = matrix(betas, nrow = n, ncol = num_boxes, byrow = TRUE)
+    intercepts_mtx = matrix(intercepts, nrow = n, ncol = num_boxes, byrow = TRUE)
+    fit_matrix = intercepts_mtx + betas_mtx * x
+    
+    # RMS calculation as in DFA (average fluctuation)
+    return(sqrt(sum((y_matrix - fit_matrix)^2) / N_total))
+  }
+  
+  # MAIN SCRIPT ------
   if (inherits(x = data, "data.frame")) {
     data = data[, 1]
   }
   if (length(data) <= box_size || any(is.na(data))) {
     alpha_hat = NA
   } else {
-    dfa_hat = DFA(data, scale = scale, box_size = box_size, m = m)    
-    est_ols = lm(log(dfa_hat[,2]) ~ log(dfa_hat[,1]))
-    alpha_hat = est_ols$coefficients[[2]]    
+    # Box size calculation (imitating DFA() function)
+    N = length(data)
+    if (scale != "F") {
+      box_sizes <- 4
+      while (tail(box_sizes, 1) + 4 < round(N/4)) {
+        box_sizes <- c(box_sizes, ceiling(scale * tail(box_sizes, 1)))
+      }
+    } else {
+      box_sizes = box_size
+    }
+
+    # y_k calculation (equal to DFA() function)
+    y_k = cumsum(data - mean(data))
+
+    # Apply to all box sizes
+    dfa_values = sapply(box_sizes, calc_fluctuation, y = y_k, N_total = N)
+
+    # Final Alpha Calculation
+    model = lm(log(dfa_values) ~ log(box_sizes))
+    alpha_hat = as.numeric(coef(model)[2])
   }
   return(alpha_hat)
 } 
