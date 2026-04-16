b48c3a43b3b0a5ef5765cf67448827ae54734b4b
jhmigueles <jairo@jhmigueles.com>
2026-04-16 17:18:06 +0200

    Move fluctuation analysis to DFA and keep only slope calculations in SSP (suggested by Vincent)

--- R/SSP.R
+++ R/SSP.R
@@ -32,82 +32,34 @@
 #- SSP(sunspot.year, scale = 1.2)
 
 SSP = function(data, scale = 2^(1/8), box_size = 4, m = 1, epochSize){
-  
-  # Helper to replicate the fluctuation analysis using sapply later
-  calc_fluctuation = function(n, y, N_total) {
-    num_boxes = N_total %/% n
-    W = n * num_boxes
-    y_truncated = y[1:W]
-    
-    # Reshape y into a matrix: each column is one box
-    y_matrix = matrix(y_truncated, nrow = n)
-    
-    # Create the x-vector for a single box: 1:n
-    x = 1:n
-    # Slope (beta) for each box: beta = cov(x,y) / var(x)
-    x_detrend = x - mean(x)
-    sum_x2 = sum(x_detrend^2)
-    
-    # betas is a vector of slopes (one for each box)
-    betas = colSums(y_matrix * x_detrend) / sum_x2
-    
-    # intercepts is a vector of intercepts (one for each box)
-    y_means = colMeans(y_matrix)
-    intercepts = y_means - (betas * mean(x))
-    
-    # Fits for every point
-    # fit_matrix[i, j] = intercepts[j] + betas[j] * x[i]
-    betas_mtx = matrix(betas, nrow = n, ncol = num_boxes, byrow = TRUE)
-    intercepts_mtx = matrix(intercepts, nrow = n, ncol = num_boxes, byrow = TRUE)
-    fit_matrix = intercepts_mtx + betas_mtx * x
-    
-    # RMS calculation as in DFA (average fluctuation)
-    return(sqrt(sum((y_matrix - fit_matrix)^2) / N_total))
-  }
-  
-  # MAIN SCRIPT ------
   if (inherits(x = data, "data.frame")) {
     data = data[, 1]
   }
+  
   if (length(data) <= box_size || any(is.na(data))) {
     return(list(alpha_overall = NA, alpha_1 = NA, alpha_2 = NA))
   } else {
-    # Box size calculation (imitating DFA() function)
-    N = length(data)
-    if (scale != "F") {
-      box_sizes <- 4
-      while (tail(box_sizes, 1) + 4 < round(N/4)) {
-        box_sizes <- c(box_sizes, ceiling(scale * tail(box_sizes, 1)))
-      }
-    } else {
-      box_sizes = box_size
-    }
-
-    # y_k calculation (equal to DFA() function)
-    y_k = cumsum(data - mean(data))
-
-    # Apply to all box sizes
-    dfa_values = sapply(box_sizes, calc_fluctuation, y = y_k, N_total = N)
+    # Calculate fluctuation values for all box sizes
+    dfa_hat = DFA(data, scale = scale, box_size = box_size, m = m)
     
-    # Convert box sizes to minutes to apply boundaries for alpha_1 and alpha_2
+    box_sizes = dfa_hat[, "box"]
+    dfa_values = dfa_hat[, "DFA"]
     box_sizes_min = box_sizes * epochSize / 60
     
-    # OUTPUT ----
+    # Overall Alpha
+    model_overall = lm(log(dfa_values) ~ log(box_sizes))
+    alpha_overall = as.numeric(coef(model_overall)[2])
     
-    # 1. Overal Alpha (SSP)
-    model = lm(log(dfa_values) ~ log(box_sizes))
-    alpha_hat = as.numeric(coef(model)[2])
-    
-    # 2. alpha_1 (<= 90 minutes)
+    # Alpha_1 (<= 90 minutes)
     idx_1 = which(box_sizes_min <= 90)
-    if (length(idx_1) >= 3) { # required at least 3 values to fit the lm
+    if (length(idx_1) >= 3) {
       model_1 = lm(log(dfa_values[idx_1]) ~ log(box_sizes[idx_1]))
       alpha_1 = as.numeric(coef(model_1)[2])
     } else {
       alpha_1 = NA
     }
     
-    # 3. Final Alpha_2 Calculation (Between 120 and 600 minutes)
+    # Alpha_2 (Between 120 and 600 minutes)
     idx_2 = which(box_sizes_min >= 120 & box_sizes_min <= 600)
     if (length(idx_2) >= 3) {
       model_2 = lm(log(dfa_values[idx_2]) ~ log(box_sizes[idx_2]))
@@ -116,5 +68,6 @@ SSP = function(data, scale = 2^(1/8), box_size = 4, m = 1, epochSize){
       alpha_2 = NA
     }
   }
-  return(list(alpha_overall = alpha_hat, alpha_1 = alpha_1, alpha_2 = alpha_2))
+  
+  return(list(alpha_overall = alpha_overall, alpha_1 = alpha_1, alpha_2 = alpha_2))
 } 
